135fcb678a56b8168cef1aefaaeb1482904c7635
leeseojune <sung07288346@gmail.com>
2021-10-24 23:00:26 +0900

    [CREATE] Project init

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/RollsRoyceApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.rollsroyce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollsRoyceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollsRoyceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/hs/entrydsm/rollsroyce/RollsRoyceApplicationTests.java
@@ -0,0 +1,14 @@
+package kr.hs.entrydsm.rollsroyce;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollsRoyceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
