fedb79b5de80307c05cf3321a0117fd854d5b2ae
leeseojune <sung07288346@gmail.com>
2021-10-25 21:46:27 +0900

    [CREATE] UserNotFoundException

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/exception/UserNotFoundException.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.rollsroyce.domain.user.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class UserNotFoundException extends RollsException {
+
+	public static RollsException EXCEPTION =
+			new UserNotFoundException();
+
+	private UserNotFoundException() {
+		super(ErrorCode.USER_NOT_FOUND);
+	}
+
+}
