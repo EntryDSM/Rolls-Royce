1db08c8fafcbc797a871e5bd9912b455e597b95b
leeseojune <sung07288346@gmail.com>
2021-10-25 21:54:11 +0900

    [UPDATE] PropertiesScan 설정

--- src/main/java/kr/hs/entrydsm/rollsroyce/RollsRoyceApplication.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/RollsRoyceApplication.java
@@ -2,8 +2,10 @@ package kr.hs.entrydsm.rollsroyce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class RollsRoyceApplication {
 
 	public static void main(String[] args) {
