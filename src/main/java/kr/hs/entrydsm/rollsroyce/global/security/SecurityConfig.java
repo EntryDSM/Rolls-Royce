77bf84dfcc1a109dd2e5b27cd5d805a4b36cb1cd
leeseojune <sung07288346@gmail.com>
2021-10-25 23:18:01 +0900

    [CREATE] SecurityConfig

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package kr.hs.entrydsm.rollsroyce.global.security;
+
+import kr.hs.entrydsm.rollsroyce.global.error.GlobalExceptionFilter;
+import kr.hs.entrydsm.rollsroyce.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final JwtTokenProvider jwtTokenProvider;
+	private final GlobalExceptionFilter globalExceptionFilter;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().and()
+				.cors().and()
+				.formLogin().disable()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http
+				.authorizeRequests()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				.anyRequest().authenticated()
+				.and().apply(new FilterConfig(jwtTokenProvider, globalExceptionFilter));
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
