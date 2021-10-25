374a69f652db5ba378068d5f630ea5453f510779
leeseojune <sung07288346@gmail.com>
2021-10-25 21:55:07 +0900

    [CREATE] InvalidTokenException

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/exception/InvalidTokenException.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.rollsroyce.global.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class InvalidTokenException extends RollsException {
+
+	public static RollsException EXCEPTION =
+			new InvalidTokenException();
+
+	private InvalidTokenException() {
+		super(ErrorCode.INVALID_TOKEN);
+	}
+
+}
