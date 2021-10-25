31dea48ecd971a5da424f103f1d6641908cecda8
leeseojune <sung07288346@gmail.com>
2021-10-25 23:10:39 +0900

    [CREATE] Web Config
    cors

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/WebConfig.java
@@ -0,0 +1,18 @@
+package kr.hs.entrydsm.rollsroyce.global.security;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT")
+				.allowedOrigins("http://localhost:3000");
+	}
+
+}
