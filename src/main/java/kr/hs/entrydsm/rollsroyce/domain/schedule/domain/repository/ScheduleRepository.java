77b6fdbae6cbd9fd830b4f8ecc9998bc54a78a1f
leeseojune <sung07288346@gmail.com>
2021-10-26 18:57:00 +0900

    [CREATE] ScheduleRepository

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/schedule/domain/repository/ScheduleRepository.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.rollsroyce.domain.schedule.domain.repository;
+
+import kr.hs.entrydsm.rollsroyce.domain.schedule.domain.Schedule;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ScheduleRepository extends CrudRepository<Schedule, Long> {
+}
