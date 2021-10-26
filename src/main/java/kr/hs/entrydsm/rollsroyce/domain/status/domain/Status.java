301b0fed0d7e3491225e58b28c8872aa546d5840
leeseojune <sung07288346@gmail.com>
2021-10-26 15:33:24 +0900

    [CREATE] Status Entity

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/status/domain/Status.java
@@ -0,0 +1,49 @@
+package kr.hs.entrydsm.rollsroyce.domain.status.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tbl_status")
+public class Status {
+
+	@Id
+	private long receiptCode;
+
+	@MapsId
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "receipt_code")
+	private User user;
+
+	private boolean isPrintedArrived;
+
+	@ColumnDefault("0")
+	private Boolean isSubmit;
+
+	private LocalDateTime submittedAt;
+
+	@Column(columnDefinition = "char(5)")
+	private String examCode;
+
+	@ColumnDefault("0")
+	@Column(nullable = false)
+	private boolean isFirstRoundPass;
+
+}
