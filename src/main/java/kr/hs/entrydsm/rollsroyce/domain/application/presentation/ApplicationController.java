2352906496d82d2bc7092cf5e99a03f042a14f43
leeseojune <sung07288346@gmail.com>
2021-10-27 02:40:38 +0900

    ⚡ :: GET /application/users

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/presentation/ApplicationController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/presentation/ApplicationController.java
@@ -3,10 +3,13 @@ package kr.hs.entrydsm.rollsroyce.domain.application.presentation;
 import javax.validation.Valid;
 
 import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.request.ChangeTypeRequest;
+import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.QueryInformationResponse;
 import kr.hs.entrydsm.rollsroyce.domain.application.service.ChangeTypeService;
+import kr.hs.entrydsm.rollsroyce.domain.application.service.QueryInformationService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +22,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationController {
 
 	private final ChangeTypeService changeTypeService;
+	private final QueryInformationService queryInformationService;
 
-	@PatchMapping("/user/type")
+	@PatchMapping("/users/type")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void changeType(@RequestBody @Valid ChangeTypeRequest request) {
 		changeTypeService.execute(request);
+	}
+
+	@GetMapping("/users")
+	public QueryInformationResponse queryInformation() {
+		return queryInformationService.execute();
 	}
 
 }
