22e03aa4c7d797093df0445192ad6d6c7bfbce3f
이준서 <ljchan71@gmail.com>
2021-10-27 08:21:46 +0900

    📑 :: ChangeIntroRequest

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/presentation/dto/request/ChangeIntroRequest.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@NoArgsConstructor
+public class ChangeIntroRequest {
+
+    @Length(max = 1_600, message = "content는 최대 1,600글자까지만 허용됩니다.")
+    private String content;
+}
