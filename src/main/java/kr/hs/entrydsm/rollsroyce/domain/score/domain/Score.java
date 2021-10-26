0e52b8f7d50f67936915720d4583a7f169c0685e
leeseojune <sung07288346@gmail.com>
2021-10-26 20:29:40 +0900

    [UPDATE] Score constructor

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/Score.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/Score.java
@@ -38,4 +38,18 @@ public class Score {
 	@Digits(integer = 3, fraction = 3)
 	private BigDecimal totalScore;
 
+	public Score(long receiptCode, ApplicationCase applicationCase) {
+		this.receiptCode = receiptCode;
+		this.volunteerScore = applicationCase.calculateVolunteerScore();
+		this.attendanceScore = applicationCase.calculateAttendanceScore();
+
+		BigDecimal[] gradeScores = applicationCase.calculateGradeScores();
+		this.thirdBeforeBeforeScore = gradeScores[0];
+		this.thirdBeforeScore = gradeScores[1];
+		this.thirdGradeScore = gradeScores[2];
+
+		this.totalGradeScore = applicationCase.calculateTotalGradeScore();
+		this.totalScore = volunteerScore.add(BigDecimal.valueOf(attendanceScore)).add(totalGradeScore);
+	}
+
 }
