4bdde37a42cd15536203d493c61b92ef19ccf748
leeseojune <sung07288346@gmail.com>
2021-10-26 20:23:45 +0900

    [CREATE] QualificationCase Dao

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/QualificationCaseRepository.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.rollsroyce.domain.score.domain.repository;
+
+import kr.hs.entrydsm.rollsroyce.domain.score.domain.QualificationCase;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface QualificationCaseRepository extends CrudRepository<QualificationCase, Long> {
+}
