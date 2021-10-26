02d1e8a012f004001312a2b433a4b37d02526c30
이승윤 <yyuunn17@naver.com>
2021-10-27 01:43:07 +0900

    ⚡️ :: 전형일자 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/schedule/presentation/ScheduleController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/schedule/presentation/ScheduleController.java
@@ -1,20 +1,31 @@
 package kr.hs.entrydsm.rollsroyce.domain.schedule.presentation;
 
+import kr.hs.entrydsm.rollsroyce.domain.schedule.presentation.dto.request.ScheduleRequest;
 import kr.hs.entrydsm.rollsroyce.domain.schedule.presentation.dto.response.SchedulesResponse;
 import kr.hs.entrydsm.rollsroyce.domain.schedule.service.GetSchedulesService;
+import kr.hs.entrydsm.rollsroyce.domain.schedule.service.UpdateSchedulesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
 public class ScheduleController {
 
     private final GetSchedulesService getSchedulesService;
+    private final UpdateSchedulesService updateSchedulesService;
 
     @GetMapping("/schedules")
     public SchedulesResponse getSchedule() {
         return getSchedulesService.execute();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/schedule")
+    public void updateSchedule(@RequestBody @Valid ScheduleRequest request) {
+        updateSchedulesService.execute(request);
     }
 
 }
---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/schedule/service/UpdateSchedulesService.java
@@ -0,0 +1,33 @@
+package kr.hs.entrydsm.rollsroyce.domain.schedule.service;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminAuthenticationFacade;
+import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminFacade;
+import kr.hs.entrydsm.rollsroyce.domain.schedule.domain.Schedule;
+import kr.hs.entrydsm.rollsroyce.domain.schedule.domain.repository.ScheduleRepository;
+import kr.hs.entrydsm.rollsroyce.domain.schedule.domain.types.Type;
+import kr.hs.entrydsm.rollsroyce.domain.schedule.exception.ScheduleNotFoundException;
+import kr.hs.entrydsm.rollsroyce.domain.schedule.presentation.dto.ScheduleDto;
+import kr.hs.entrydsm.rollsroyce.domain.schedule.presentation.dto.request.ScheduleRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UpdateSchedulesService {
+
+    private final ScheduleRepository scheduleRepository;
+
+    private final AdminFacade adminFacade;
+    private final AdminAuthenticationFacade authenticationFacade;
+
+    public void execute(ScheduleRequest request) {
+        adminFacade.getRootAdmin(authenticationFacade.getEmail());
+
+        for (ScheduleDto schedule : request.getSchedules()) {
+            Schedule updateSchedule = scheduleRepository.findByType(Type.valueOf(schedule.getType()));
+            if (updateSchedule == null) throw ScheduleNotFoundException.EXCEPTION;
+            scheduleRepository.save(updateSchedule.updateDate(schedule.getDate()));
+        }
+    }
+
+}
