d3fe783f58c046ecf46700f43e34b72b8bfc0bd2
leeseojune <sung07288346@gmail.com>
2021-10-26 20:12:49 +0900

    [CREATE] Qualification Dao

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/domain/repository/QualificationRepositroy.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.rollsroyce.domain.application.domain.repository;
+
+import kr.hs.entrydsm.rollsroyce.domain.application.domain.Qualification;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface QualificationRepositroy extends CrudRepository<Qualification, Long> {
+}
