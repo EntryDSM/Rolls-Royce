6cac598fe278e4caca8aa439ffd00a605057b1ec
이승윤 <yyuunn17@naver.com>
2021-10-27 00:53:43 +0900

    📝 :: 403 Exception 추가

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/exception/AdminNotAccessibleException.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.rollsroyce.domain.admin.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class AdminNotAccessibleException extends RollsException {
+
+    public static RollsException EXCEPTION =
+            new AdminNotAccessibleException();
+
+    private AdminNotAccessibleException() {
+        super(ErrorCode.ADMIN_NOT_FOUND);
+    }
+
+}
