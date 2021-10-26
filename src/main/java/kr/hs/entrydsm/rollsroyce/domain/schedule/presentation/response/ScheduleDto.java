1715b43f473bf54a57414f280f79a68a6ed68b5f
이승윤 <yyuunn17@naver.com>
2021-10-26 23:01:15 +0900

    📝:: response 및 controller 추가

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/schedule/presentation/response/ScheduleDto.java
@@ -0,0 +1,14 @@
+package kr.hs.entrydsm.rollsroyce.domain.schedule.presentation.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ScheduleDto {
+
+    private final String type;
+
+    private final String date;
+
+}
---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/schedule/presentation/response/SchedulesResponse.java
@@ -0,0 +1,18 @@
+package kr.hs.entrydsm.rollsroyce.domain.schedule.presentation.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class SchedulesResponse {
+
+    private final List<ScheduleDto> schedules;
+
+    private final String currentStatus;
+
+}
