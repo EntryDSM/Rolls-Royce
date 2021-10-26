1510f15f80d5592da08745c0ae7de4651c982ea5
leeseojune <sung07288346@gmail.com>
2021-10-26 22:02:10 +0900

    📑 :: AuthCodeLimit Dao

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/AuthCodeLimitRepository.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.rollsroyce.domain.user.domain.repository;
+
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.AuthCodeLimit;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthCodeLimitRepository extends CrudRepository<AuthCodeLimit, String> {
+}
