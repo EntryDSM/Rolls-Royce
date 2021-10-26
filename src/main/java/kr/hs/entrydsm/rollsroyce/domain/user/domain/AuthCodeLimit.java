5c8a6fdf00241014f839742069d47961598acfa8
leeseojune <sung07288346@gmail.com>
2021-10-26 22:02:02 +0900

    📑 :: AuthCodeLimit Entity

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/AuthCodeLimit.java
@@ -0,0 +1,29 @@
+package kr.hs.entrydsm.rollsroyce.domain.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@AllArgsConstructor
+@RedisHash
+public class AuthCodeLimit {
+
+	@Id
+	private final String email;
+
+	private int count;
+
+	@TimeToLive
+	private long ttl;
+
+	public AuthCodeLimit updateAuthCode(long ttl) {
+		this.count++;
+		this.ttl = ttl;
+		return this;
+	}
+
+}
