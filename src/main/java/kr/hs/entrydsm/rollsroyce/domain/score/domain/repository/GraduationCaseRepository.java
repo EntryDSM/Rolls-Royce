e62c3981aa2422a064766a99f6b758fb044c30d6
leeseojune <sung07288346@gmail.com>
2021-10-26 20:27:56 +0900

    [CREATE] GraduationCase Dao

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/GraduationCaseRepository.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.rollsroyce.domain.score.domain.repository;
+
+import kr.hs.entrydsm.rollsroyce.domain.score.domain.GraduationCase;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface GraduationCaseRepository extends CrudRepository<GraduationCase, Long> {
+}
