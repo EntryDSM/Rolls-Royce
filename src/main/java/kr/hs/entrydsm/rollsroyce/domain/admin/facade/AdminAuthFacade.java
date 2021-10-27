7cd8a0477d2b1749f3c59a6bddb2592e04196630
이승윤 <yyuunn17@naver.com>
2021-10-27 10:27:55 +0900

    ♻️ :: 토큰 발급 부분 facade로 분리
    로그인 서비스와 토큰재발급 서비스에서 공통적으로 사용될 부분이라 분리함

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/facade/AdminAuthFacade.java
@@ -0,0 +1,32 @@
+package kr.hs.entrydsm.rollsroyce.domain.admin.facade;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.TokenResponse;
+import kr.hs.entrydsm.rollsroyce.domain.refresh_token.domain.RefreshToken;
+import kr.hs.entrydsm.rollsroyce.domain.refresh_token.domain.repository.RefreshTokenRepository;
+import kr.hs.entrydsm.rollsroyce.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class AdminAuthFacade {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Value("${auth.jwt.refreshExp}")
+    private long ttl;
+
+    private static final String ROLE = "admin";
+
+    public TokenResponse getToken(String id) {
+        String refreshToken = jwtTokenProvider.generateRefreshToken(id, ROLE);
+        refreshTokenRepository.save(new RefreshToken(id, refreshToken, ttl));
+
+        String accessToken = jwtTokenProvider.generateAccessToken(id, ROLE);
+        return new TokenResponse(accessToken, refreshToken);
+    }
+
+}
