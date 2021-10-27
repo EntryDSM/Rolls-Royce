55908c8db9f33a1bb59d1113bc586f71fe9a620f
김범진 <sdpthf@naver.com>
2021-10-28 01:27:06 +0900

    📝 :: UserAlreadyExistsException

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/exception/UserAlreadyExistsException.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.rollsroyce.domain.user.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class UserAlreadyExistsException extends RollsException {
+
+    public static RollsException EXCEPTION =
+            new UserAlreadyExistsException();
+
+    public UserAlreadyExistsException() {
+        super(ErrorCode.USER_ALREADY_EXISTS);
+    }
+
+}
