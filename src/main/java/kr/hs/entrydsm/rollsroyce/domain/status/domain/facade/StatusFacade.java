c831a1516071fc7c4c3590488c4bdf7743f3387c
leeseojune <sung07288346@gmail.com>
2021-10-27 19:40:12 +0900

    📑 :: getStatusByReceiptCode

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/status/domain/facade/StatusFacade.java
@@ -0,0 +1,21 @@
+package kr.hs.entrydsm.rollsroyce.domain.status.domain.facade;
+
+import kr.hs.entrydsm.rollsroyce.domain.status.domain.Status;
+import kr.hs.entrydsm.rollsroyce.domain.status.domain.repository.StatusRepository;
+import kr.hs.entrydsm.rollsroyce.domain.status.exception.StatusNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class StatusFacade {
+
+	private final StatusRepository statusRepository;
+
+	public Status getStatusByReceiptCode(long receiptCode) {
+		return statusRepository.findById(receiptCode)
+				.orElseThrow(() -> StatusNotFoundException.EXCEPTION);
+	}
+
+}
