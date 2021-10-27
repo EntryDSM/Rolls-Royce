8571918ce4837e8c7d1c0dac69f711a423b85bf3
김범진 <sdpthf@naver.com>
2021-10-28 01:30:05 +0900

    📝 :: isAlreadyExists

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
@@ -4,5 +4,8 @@ import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
