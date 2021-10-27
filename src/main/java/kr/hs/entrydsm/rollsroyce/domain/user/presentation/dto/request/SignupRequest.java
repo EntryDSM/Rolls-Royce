9079eb222004c8c2190e48afa926dcc1030c59aa
김범진 <sdpthf@naver.com>
2021-10-28 08:13:22 +0900

    ⚡️ :: 메시지 추가

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/dto/request/SignupRequest.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/dto/request/SignupRequest.java
@@ -12,15 +12,16 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 public class SignupRequest {
 
-    @Email
-    @NotBlank
+    @Email(message = "email 형식을 지켜야 합니다.")
+    @NotBlank(message = "email은 공백이 될 수 없습니다.")
     private String email;
 
-    @NotBlank
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[~!@#*_])[a-zA-Z0-9~!@#*_]{8,32}$")
+    @NotBlank(message = "password는 공백이 될 수 없습니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,-./:;<=>?@＼^_`{|}~]){8,32}$",
+            message = "password는 소문자, 숫자, 특수문자가 포함되어야 합니다.")
     private String password;
 
-    @NotBlank
+    @NotBlank(message = "name은 공백이 될 수 없습니다.")
     @Length(min = 1, max = 5, message = "INVALID NAME")
     private String name;
 
