28263f88b74eea2c77a049420915c701428ee1ab
김범진 <sdpthf@naver.com>
2021-10-28 01:38:36 +0900

    📝 :: UserController

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/UserController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/UserController.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.rollsroyce.domain.user.presentation;
 
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.SignupRequest;
+import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.response.TokenResponse;
 import kr.hs.entrydsm.rollsroyce.domain.user.service.UserSignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
@@ -19,8 +19,8 @@ public class UserController {
 
     @PostMapping("/user")
     @ResponseStatus(HttpStatus.CREATED)
-    public void signup(@RequestBody @Valid SignupRequest request) {
-        signupService.execute(request);
+    public TokenResponse signup(@RequestBody @Valid SignupRequest request) {
+        return signupService.execute(request);
     }
 
 }
