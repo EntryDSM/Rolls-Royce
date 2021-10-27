bcb8f5f4958c61495d8dd7650279469a3785fea9
leeseojune <sung07288346@gmail.com>
2021-10-27 18:27:11 +0900

    ⚡ :: getSchoolByName

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/school/facade/SchoolFacade.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/school/facade/SchoolFacade.java
@@ -5,6 +5,8 @@ import kr.hs.entrydsm.rollsroyce.domain.school.domain.repository.SchoolRepositor
 import kr.hs.entrydsm.rollsroyce.domain.school.exception.SchoolNotFoundException;
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +18,11 @@ public class SchoolFacade {
 	public School getSchoolByCode(String code) {
 		return schoolRepository.findById(code)
 				.orElseThrow(() -> SchoolNotFoundException.EXCEPTION);
+	}
+
+	public Page<School> getSchoolByName(String name, int size, int page) {
+		return schoolRepository
+				.findByNameContaining(name, PageRequest.of(page, size));
 	}
 
 }
