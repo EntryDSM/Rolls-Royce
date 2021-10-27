ffe75138d190345b7113e3d6bb6b4d25301c2a14
이승윤 <yyuunn17@naver.com>
2021-10-27 18:48:58 +0900

    ⚡️ :: 최종제출 취소 api

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/CancelApplicationSubmitService.java
@@ -0,0 +1,36 @@
+package kr.hs.entrydsm.rollsroyce.domain.admin.service;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.domain.types.Role;
+import kr.hs.entrydsm.rollsroyce.domain.admin.exception.AdminNotAccessibleException;
+import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminAuthenticationFacade;
+import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminFacade;
+import kr.hs.entrydsm.rollsroyce.domain.status.domain.Status;
+import kr.hs.entrydsm.rollsroyce.domain.status.domain.repository.StatusRepository;
+import kr.hs.entrydsm.rollsroyce.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class CancelApplicationSubmitService {
+
+    private final StatusRepository statusRepository;
+
+    private final AdminAuthenticationFacade authenticationFacade;
+
+    private final AdminFacade adminFacade;
+
+    @Transactional
+    public void execute(long receiptCode) {
+        Status status = statusRepository.findById(receiptCode)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        if (!adminFacade.getAdminRole(authenticationFacade.getEmail()).equals(Role.ROLE_CONFIRM_FEE)) {
+            status.cancelIsSumitted();
+        } else {
+            throw AdminNotAccessibleException.EXCEPTION;
+        }
+    }
+
+}
