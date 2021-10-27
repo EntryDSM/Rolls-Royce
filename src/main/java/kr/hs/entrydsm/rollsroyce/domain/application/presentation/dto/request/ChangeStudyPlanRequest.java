0636e202a9a7fe6178694198554f955598c8cda9
정대현 <eogus_2513@dsm.hs.kr>
2021-10-28 02:04:46 +0900

    📑 :: ChangeStudyPlanRequest

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/presentation/dto/request/ChangeStudyPlanRequest.java
@@ -0,0 +1,14 @@
+package kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.request;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@NoArgsConstructor
+public class ChangeStudyPlanRequest {
+
+    @Length(max = 1_600, message = "content는 최대 1,600글자까지만 허용됩니다.")
+    private String content;
+}
