017b446b276d66aae75e17283ef7773a20c6b8a2
김범진 <sdpthf@naver.com>
2021-10-28 02:14:05 +0900

    🐛 :: Import List

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
@@ -4,6 +4,7 @@ import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
