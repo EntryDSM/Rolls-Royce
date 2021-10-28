f55922cebc7fabf62a5d3faf0590d27d311516a8
김범진 <sdpthf@naver.com>
2021-10-28 16:45:48 +0900

    ⚡️ :: 코드 요청 횟수 제한

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/SendAuthCodeService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/SendAuthCodeService.java
@@ -1,13 +1,14 @@
 package kr.hs.entrydsm.rollsroyce.domain.user.service;
 
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.AuthCode;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.AuthCodeLimit;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.AuthCodeLimitRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.AuthCodeRepository;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserRepository;
+import kr.hs.entrydsm.rollsroyce.domain.user.exception.AuthCodeRequestOverLimitException;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.UserAlreadyExistsException;
 import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserAuthCodeFacade;
 import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.SendEmailRequest;
-import kr.hs.entrydsm.rollsroyce.global.security.jwt.JwtProperties;
 import kr.hs.entrydsm.rollsroyce.global.utils.ses.SESUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,16 +16,20 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class SendAuthCodeService {
 
+    private static final long authCodeTTL = 3 * 60;
+    private static final long authCodeLimit = 5;
+
     private final SESUtil sesUtil;
     private final UserFacade userFacade;
-    private final JwtProperties jwtProperties;
     private final UserAuthCodeFacade authCodeFacade;
     private final AuthCodeRepository authCodeRepository;
+    private final AuthCodeLimitRepository authCodeLimitRepository;
 
     @Transactional
     public void execute(SendEmailRequest request) {
@@ -34,19 +39,41 @@ public class SendAuthCodeService {
         Map<String, String> params = new HashMap<>();
         params.put("code", code);
 
-        if(userFacade.isAlreadyExists(email)) {
-            throw UserAlreadyExistsException.EXCEPTION;
-        }
+        if(isOverLimit(email))
+            throw AuthCodeRequestOverLimitException.EXCEPTION;
 
-        authCodeRepository.findById(request.getEmail())
+        if(userFacade.isAlreadyExists(email))
+            throw UserAlreadyExistsException.EXCEPTION;
+
+        authCodeRepository.findById(email)
                 .filter(authcode -> sesUtil.sendMessage(email, "RollsRoyceEmailTemplate", params))
-                .map(authCode -> authCode.updateAuthCode(code, jwtProperties.getRefreshExp()))
+                .map(authCode -> authCode.updateAuthCode(code, authCodeTTL))
                 .orElseGet(() -> authCodeRepository.save(AuthCode.builder()
+                            .email(email)
+                            .code(code)
+                            .isVerified(false)
+                            .ttl(authCodeTTL)
+                            .build()));
+
+        authCodeLimitRepository.findById(email)
+                .map(authCodeLimit -> authCodeLimit.updateAuthCode(authCodeTTL))
+                .orElseGet(() -> authCodeLimitRepository.save(AuthCodeLimit.builder()
                         .email(email)
-                        .code(code)
-                        .isVerified(false)
-                        .ttl(jwtProperties.getRefreshExp())
-                        .build()))  ;
+                        .count(1)
+                        .ttl(authCodeTTL)
+                        .build()));
+    }
+
+    private boolean isOverLimit(String email) {
+        return !authCodeLimitRepository.findById(email)
+                .filter(limit -> limit.getCount() < authCodeLimit)
+                .map(authCodeLimit -> authCodeLimit.updateAuthCode(authCodeTTL))
+                .or(() -> Optional.of(authCodeLimitRepository.save(AuthCodeLimit.builder()
+                        .email(email)
+                        .count(1)
+                        .ttl(authCodeTTL)
+                        .build())))
+                .isPresent();
     }
 
 }
