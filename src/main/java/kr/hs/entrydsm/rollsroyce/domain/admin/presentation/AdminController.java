5428aecc6ae7647b4a43691669d0331595bbc2bb
이승윤 <yyuunn17@naver.com>
2021-10-28 23:45:25 +0900

    ⚡️ :: 지원자 목록(검색 및 필터링)

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/AdminController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/AdminController.java
@@ -2,9 +2,11 @@ package kr.hs.entrydsm.rollsroyce.domain.admin.presentation;
 
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.CheckPasswordRequest;
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.LoginRequest;
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.ApplicantsResponse;
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.TokenResponse;
 import kr.hs.entrydsm.rollsroyce.domain.admin.service.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +27,7 @@ public class AdminController {
     private final ApplicantsExcelService applicantsExcelService;
     private final UpdateApplicantIsPaidService updateApplicantIsPaidService;
     private final UpdateIsPrintsArrivedService updateIsPrintsArrivedService;
+    private final GetApplicantsService getApplicantsService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/data")
@@ -70,6 +73,22 @@ public class AdminController {
     @PatchMapping("/applicant/paid/{receipt-code}")
     public void updateApplicantIsPaidStatus(@PathVariable("receipt-code") long receiptCode) {
         updateApplicantIsPaidService.execute(receiptCode);
+    }
+
+    @GetMapping( "/excel/applicants")
+    public ApplicantsResponse getApplicants(Pageable page,
+                                            @RequestParam(name = "receipt_code", defaultValue = "%") String receiptCode,
+                                            @RequestParam(defaultValue = "%") String name,
+                                            @RequestParam(name = "telephone", defaultValue = "%") String schoolName,
+                                            @RequestParam(name = "is_daejeon") boolean isDaejeon,
+                                            @RequestParam(name = "is_nationwide") boolean isNationwide,
+                                            @RequestParam(name = "is_common") boolean isCommon,
+                                            @RequestParam(name = "is_meister") boolean isMeister,
+                                            @RequestParam(name = "is_social") boolean isSocial,
+                                            @RequestParam(name = "is_in") boolean inOfHeadcount,
+                                            @RequestParam(name = "is_out") boolean outOfHeadcount,
+                                            @RequestParam(name = "is_prints_arrived", required = false) boolean isSubmitted) {
+        return getApplicantsService.execute(page, receiptCode, name, schoolName, isDaejeon, isNationwide, isCommon, isMeister, isSocial, inOfHeadcount, outOfHeadcount, isSubmitted);
     }
 
 }
---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/GetApplicantsService.java
@@ -0,0 +1,52 @@
+package kr.hs.entrydsm.rollsroyce.domain.admin.service;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.ApplicantsResponse;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserCustomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class GetApplicantsService {
+
+    private final UserCustomRepository userCustomRepository;
+
+    public ApplicantsResponse execute(Pageable page, String receiptCode, String name, String schoolName,
+                                      boolean isDaejeon, boolean isNationwide,
+                                      boolean isCommon, boolean isMeister, boolean isSocial,
+                                      boolean inOfHeadcount, boolean outOfHeadcount, boolean isSubmitted) {
+        Boolean isDaejeonQuery;
+        if (isDaejeon == isNationwide) isDaejeonQuery = null;
+        else isDaejeonQuery = isDaejeon;
+
+        Page<User> users = userCustomRepository.findAllByUserInfo(receiptCode, isDaejeonQuery,
+                "%"+schoolName+ "%", "%"+name+"%",
+                inOfHeadcount, outOfHeadcount,
+                isCommon, isMeister, isSocial, isSubmitted, page);
+
+        return ApplicantsResponse.builder()
+                .totalElements(users.getTotalElements())
+                .totalPages(users.getTotalPages())
+                .applicants(
+                        users.stream().map(
+                                user -> ApplicantsResponse.ApplicantDto.builder()
+                                        .receiptCode(user.getReceiptCode())
+                                        .name(user.getName())
+                                        .email(user.getEmail())
+                                        .isDaejeon(user.getIsDaejeon())
+                                        .applicationType(user.getApplicationType().name())
+                                        .isPrintsArrived(user.getStatus().getIsPrintsArrived())
+                                        .isSubmitted(user.getStatus().getIsSubmitted())
+                                        .build()
+                        )
+                        .collect(Collectors.toList())
+                )
+                .build();
+    }
+
+}
