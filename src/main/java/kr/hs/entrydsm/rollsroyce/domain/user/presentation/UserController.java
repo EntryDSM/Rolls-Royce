8e8c2d10c9cfcb19b5822b30af637fb1af6992f9
김범진 <sdpthf@naver.com>
2021-10-28 20:19:11 +0900

    ⚡️ :: Verify Email Controller

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/UserController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/UserController.java
@@ -2,15 +2,15 @@ package kr.hs.entrydsm.rollsroyce.domain.user.presentation;
 
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.SendEmailRequest;
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.SignupRequest;
+import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.VerifyAuthCodeRequest;
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.response.TokenResponse;
 import kr.hs.entrydsm.rollsroyce.domain.user.service.SendAuthCodeService;
 import kr.hs.entrydsm.rollsroyce.domain.user.service.UserSignupService;
+import kr.hs.entrydsm.rollsroyce.domain.user.service.VerifyAuthCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
 
 @RequiredArgsConstructor
@@ -19,6 +19,7 @@ public class UserController {
 
     private final UserSignupService signupService;
     private final SendAuthCodeService sendAuthCodeService;
+    private final VerifyAuthCodeService verifyAuthCodeService;
 
     @PostMapping("/user")
     @ResponseStatus(HttpStatus.CREATED)
@@ -29,6 +30,11 @@ public class UserController {
     @PostMapping("/email/verify")
     public void sendEmail(@RequestBody @Valid SendEmailRequest request) {
         sendAuthCodeService.execute(request);
+    }
+
+    @PutMapping("/email/verify")
+    public void verifyEmail(@RequestBody @Valid VerifyAuthCodeRequest request) {
+        verifyAuthCodeService.execute(request);
     }
 
 }
