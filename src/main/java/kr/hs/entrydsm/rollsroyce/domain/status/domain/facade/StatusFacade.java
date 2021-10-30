b1e0c6dabee01c0947a2208cda105a41d2bc77b9
김범진 <sdpthf@naver.com>
2021-10-31 00:07:35 +0900

    ⚡️ :: 현재 제출 여부 불러오기

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/status/domain/facade/StatusFacade.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/status/domain/facade/StatusFacade.java
@@ -3,6 +3,7 @@ package kr.hs.entrydsm.rollsroyce.domain.status.domain.facade;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.Status;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.repository.StatusRepository;
 import kr.hs.entrydsm.rollsroyce.domain.status.exception.StatusNotFoundException;
+import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Component;
@@ -11,11 +12,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class StatusFacade {
 
+	private final UserFacade userFacade;
 	private final StatusRepository statusRepository;
 
 	public Status getStatusByReceiptCode(long receiptCode) {
 		return statusRepository.findById(receiptCode)
 				.orElseThrow(() -> StatusNotFoundException.EXCEPTION);
+	}
+
+	public boolean getCurrentIsSubmitted() {
+		return getStatusByReceiptCode(userFacade.getCurrentReceiptCode())
+				.getIsSubmitted();
 	}
 
 }
