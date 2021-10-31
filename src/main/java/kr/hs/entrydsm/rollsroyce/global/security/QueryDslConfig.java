5ad584e1fea7cf444e3c503abd26415025c7c3b4
이승윤 <yyuunn17@naver.com>
2021-11-01 02:24:38 +0900

    📑 :: QueryDslConfig 추가

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/QueryDslConfig.java
@@ -0,0 +1,21 @@
+package kr.hs.entrydsm.rollsroyce.global.security;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jPaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
