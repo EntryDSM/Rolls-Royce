179be694e8175beb7f482e467eb08ef72868e977
leeseojune <sung07288346@gmail.com>
2021-11-01 00:39:59 +0900

    ⚡ :: import 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/presentation/ApplicationController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/presentation/ApplicationController.java
@@ -1,66 +1,42 @@
 package kr.hs.entrydsm.rollsroyce.domain.application.presentation;
 
-import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.request.*;
-import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.*;
-import kr.hs.entrydsm.rollsroyce.domain.application.service.*;
+import javax.validation.Valid;
+
+import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.request.ChangeIntroduceRequest;
+import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.request.ChangeStudyPlanRequest;
+import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.QueryIntroduceResponse;
+import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.QuerySchoolResponse;
+import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.QueryStudyPlanResponse;
+import kr.hs.entrydsm.rollsroyce.domain.application.service.ChangeIntroduceService;
+import kr.hs.entrydsm.rollsroyce.domain.application.service.ChangeStudyPlanService;
+import kr.hs.entrydsm.rollsroyce.domain.application.service.FinalSubmitService;
+import kr.hs.entrydsm.rollsroyce.domain.application.service.QueryIntroduceService;
+import kr.hs.entrydsm.rollsroyce.domain.application.service.QuerySchoolService;
+import kr.hs.entrydsm.rollsroyce.domain.application.service.QueryStudyPlanService;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/application")
 @RequiredArgsConstructor
 public class ApplicationController {
 
-    private final ChangeTypeService changeTypeService;
-    private final ChangeInformationService changeInformationService;
-    private final QueryInformationService queryInformationService;
-    private final QueryTypeService queryTypeService;
-    private final ChangeGraduationInformationService changeGraduationInformationService;
-    private final QueryGraduationInformationService queryGraduationInformationService;
     private final ChangeIntroduceService changeIntroduceService;
     private final QueryIntroduceService queryIntroduceService;
     private final FinalSubmitService finalSubmitService;
     private final QuerySchoolService querySchoolService;
     private final ChangeStudyPlanService changeStudyPlanService;
     private final QueryStudyPlanService queryStudyPlanService;
-
-    @PatchMapping("/users/type")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void changeType(@RequestBody @Valid ChangeTypeRequest request) {
-        changeTypeService.execute(request);
-    }
-
-    @GetMapping("/user/type")
-    public QueryTypeResponse queryType() {
-        return queryTypeService.execute();
-    }
-
-    @GetMapping("/users")
-    public QueryInformationResponse queryInformation() {
-        return queryInformationService.execute();
-    }
-
-    @PatchMapping("/users")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void changeInformation(@RequestBody @Valid ChangeInformationRequest request) {
-        changeInformationService.execute(request);
-    }
-
-    @PatchMapping("/users/graduation")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void changeGraduationInformation(
-            @RequestBody @Valid ChangeGraduationInformationRequest request) {
-        changeGraduationInformationService.execute(request);
-    }
-
-    @GetMapping("/users/graduation")
-    public QueryGraduationInformationResponse queryGraduationInformation() {
-        return queryGraduationInformationService.execute();
-    }
 
     @PatchMapping("/intro")
     public void changeIntroduce(@RequestBody @Valid ChangeIntroduceRequest request) {
