ddcec7a15365d8469c0823846a3a593c6c153712
leeseojune <sung07288346@gmail.com>
2021-11-01 02:40:20 +0900

    📑 :: BadFileExtensionException

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/exception/BadFileExtensionException.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.rollsroyce.global.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class BadFileExtensionException extends RollsException {
+
+	public static RollsException EXCEPTION =
+			new BadFileExtensionException();
+
+	private BadFileExtensionException() {
+		super(ErrorCode.BAD_FILE_EXTENSION);
+	}
+
+}
