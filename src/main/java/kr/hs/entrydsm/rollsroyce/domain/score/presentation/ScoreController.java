a4a97aa753d6b79c111ff862a3e729ff70a4f877
정대현 <eogus_2513@dsm.hs.kr>
2021-11-01 02:21:27 +0900

    📑 :: ScoreController

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/ScoreController.java
@@ -0,0 +1,9 @@
+package kr.hs.entrydsm.rollsroyce.domain.score.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ScoreController {
+}
