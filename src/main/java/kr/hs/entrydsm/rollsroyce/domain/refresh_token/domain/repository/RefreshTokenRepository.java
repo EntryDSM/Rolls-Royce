539b9b3d64e40afccf0960194fdbf825622d62ab
이준서 <ljchan71@gmail.com>
2021-11-01 00:43:35 +0900

    ⚡ :: findByToken

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/refresh_token/domain/repository/RefreshTokenRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/refresh_token/domain/repository/RefreshTokenRepository.java
@@ -4,5 +4,8 @@ import kr.hs.entrydsm.rollsroyce.domain.refresh_token.domain.RefreshToken;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByToken(String token);
 }
