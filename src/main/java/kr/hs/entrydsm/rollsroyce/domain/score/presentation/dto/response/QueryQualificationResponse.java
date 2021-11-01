bbbe46d61c14bafd0c68d735504778ec32afea42
정대현 <eogus_2513@dsm.hs.kr>
2021-11-02 01:25:08 +0900

    📑 :: QueryQualificationResponse

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/dto/response/QueryQualificationResponse.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.response;
+
+import kr.hs.entrydsm.rollsroyce.domain.score.domain.QualificationCase;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class QueryQualificationResponse {
+    private final BigDecimal averageScore;
+
+    public QueryQualificationResponse(QualificationCase qualificationExamCase) {
+        this.averageScore = qualificationExamCase.getAverageScore();
+    }
+}
