4f06ea1593c193e92b2548c8957f80dd40466673
정대현 <eogus_2513@dsm.hs.kr>
2021-11-01 10:11:33 +0900

    ⚡ :: AllArgsConstructor로 변경

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/dto/response/QueryGraduationResponse.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/dto/response/QueryGraduationResponse.java
@@ -1,10 +1,13 @@
 package kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.response;
 
 import kr.hs.entrydsm.rollsroyce.domain.score.domain.GraduationCase;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class QueryGraduationResponse {
+
     private final int volunteerTime;
     private final int dayAbsenceCount;
     private final int lectureAbsenceCount;
@@ -17,21 +20,4 @@ public class QueryGraduationResponse {
     private final String scienceGrade;
     private final String englishGrade;
     private final String techAndHomeGrade;
-
-    public QueryGraduationResponse(GraduationCase graduationCase) {
-        this.volunteerTime = graduationCase.getVolunteerTime();
-
-        this.dayAbsenceCount = graduationCase.getDayAbsenceCount();
-        this.lectureAbsenceCount = graduationCase.getLectureAbsenceCount();
-        this.latenessCount = graduationCase.getLatenessCount();
-        this.earlyLeaveCount = graduationCase.getEarlyLeaveCount();
-
-        this.koreanGrade = graduationCase.getKoreanGrade();
-        this.socialGrade = graduationCase.getSocialGrade();
-        this.historyGrade = graduationCase.getHistoryGrade();
-        this.mathGrade = graduationCase.getMathGrade();
-        this.scienceGrade = graduationCase.getScienceGrade();
-        this.englishGrade = graduationCase.getEnglishGrade();
-        this.techAndHomeGrade = graduationCase.getTechAndHomeGrade();
-    }
 }
