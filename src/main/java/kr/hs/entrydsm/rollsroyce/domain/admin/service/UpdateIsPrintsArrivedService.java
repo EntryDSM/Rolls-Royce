625f8c52872894100b36711ce1a40983a4cb5356
이승윤 <yyuunn17@naver.com>
2021-11-02 01:07:07 +0900

    📑 :: 원서도착/원서미도착 시에 이메일 보내주기

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/UpdateIsPrintsArrivedService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/UpdateIsPrintsArrivedService.java
@@ -4,27 +4,46 @@ import kr.hs.entrydsm.rollsroyce.domain.admin.domain.types.Role;
 import kr.hs.entrydsm.rollsroyce.domain.admin.exception.AdminNotAccessibleException;
 import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminAuthenticationFacade;
 import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminFacade;
-import kr.hs.entrydsm.rollsroyce.domain.status.domain.Status;
-import kr.hs.entrydsm.rollsroyce.domain.status.domain.facade.StatusFacade;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserRepository;
+import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
+import kr.hs.entrydsm.rollsroyce.global.utils.ses.SESUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class UpdateIsPrintsArrivedService {
 
+    private final UserRepository userRepository;
+
     private final AdminAuthenticationFacade authenticationFacade;
 
     private final AdminFacade adminFacade;
-    private final StatusFacade statusFacade;
+    private final UserFacade userFacade;
+
+    private final SESUtil sesUtil;
 
     @Transactional
     public void execute(long receiptCode) {
-        Status status = statusFacade.getStatusByReceiptCode(receiptCode);
+        User user = userFacade.getUserByCode(receiptCode);
 
         if (!(adminFacade.getAdminRole(authenticationFacade.getEmail()) == Role.ROLE_CONFIRM_FEE)) {
-            status.updateIsPrintsArrived();
+            String template;
+            if (user.getStatus().getIsPrintsArrived()) template = "PRINTED_NOT_ARRIVED";
+            else template = "PRINTED_ARRIVED";
+
+            user.getStatus().updateIsPrintsArrived();
+            userRepository.save(user);
+
+            Map<String, String> params = new HashMap<>();
+            params.put("name", user.getName());
+
+            sesUtil.sendMessage(user.getEmail(), template, params);
         } else {
             throw AdminNotAccessibleException.EXCEPTION;
         }
