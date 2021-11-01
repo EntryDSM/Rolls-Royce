dcb34a2fb8e089051a8e84abc5fa095dd8f379a0
정대현 <eogus_2513@dsm.hs.kr>
2021-11-01 16:37:12 +0900

    ⚡ :: findByReceiptCode추가

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/QualificationCaseRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/QualificationCaseRepository.java
@@ -1,8 +1,10 @@
 package kr.hs.entrydsm.rollsroyce.domain.score.domain.repository;
 
 import kr.hs.entrydsm.rollsroyce.domain.score.domain.QualificationCase;
-
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface QualificationCaseRepository extends CrudRepository<QualificationCase, Long> {
+    Optional<QualificationCase> findByReceiptCode(long receiptCode);
 }
