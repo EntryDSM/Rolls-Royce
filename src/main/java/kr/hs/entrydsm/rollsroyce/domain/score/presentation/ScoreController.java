e20e3bf1050261935596dcee7ddce59107a4a254
정대현 <eogus_2513@dsm.hs.kr>
2021-11-02 17:37:13 +0900

    ⚡ :: @RequestMapping

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/ScoreController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/ScoreController.java
@@ -6,10 +6,12 @@ import kr.hs.entrydsm.rollsroyce.domain.score.service.QueryGraduationService;
 import kr.hs.entrydsm.rollsroyce.domain.score.service.QueryQualificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/score")
 public class ScoreController {
 
     private final QueryGraduationService queryGraduationService;
