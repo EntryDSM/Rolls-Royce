f667f696ad837d96916be9f4bdff2c21b5cd165a
이준서 <ljchan71@gmail.com>
2021-11-03 01:28:52 +0900

    ⚡️ :: isGraduate/isProspectiveGraduate

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/User.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/User.java
@@ -1,20 +1,5 @@
 package kr.hs.entrydsm.rollsroyce.domain.user.domain;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Objects;
-
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
-
 import com.querydsl.core.annotations.QueryEntity;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.Application;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.Graduation;
@@ -22,17 +7,18 @@ import kr.hs.entrydsm.rollsroyce.domain.application.domain.Qualification;
 import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.QueryInformationResponse;
 import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.QueryTypeResponse;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.Status;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationRemark;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.EducationalStatus;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.HeadCount;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.Sex;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.*;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.ApplicationNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @Builder
@@ -224,6 +210,14 @@ public class User {
 	public User updatePassword(String password) {
 		this.password = password;
 		return this;
+	}
+
+	public boolean isGraduate() {
+		return educationalStatus.equals(EducationalStatus.GRADUATE);
+	}
+
+	public boolean isProspectiveGraduate() {
+		return educationalStatus.equals(EducationalStatus.PROSPECTIVE_GRADUATE);
 	}
 
 }
