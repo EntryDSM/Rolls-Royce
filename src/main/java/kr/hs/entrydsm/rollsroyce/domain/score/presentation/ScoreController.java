c701cbc827e47ca226717c2b07358489fff676fa
이준서 <ljchan71@gmail.com>
2021-11-03 01:26:21 +0900

    ⚡️ :: updateGraduation

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/ScoreController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/ScoreController.java
@@ -1,19 +1,29 @@
 package kr.hs.entrydsm.rollsroyce.domain.score.presentation;
 
+import kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.request.UpdateGraduationRequest;
 import kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.response.QueryGraduationResponse;
 import kr.hs.entrydsm.rollsroyce.domain.score.service.QueryGraduationService;
+import kr.hs.entrydsm.rollsroyce.domain.score.service.UpdateGraduationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/score")
 public class ScoreController {
 
     private final QueryGraduationService queryGraduationService;
+    private final UpdateGraduationService updateGraduationService;
 
     @GetMapping("/graduation")
     public QueryGraduationResponse queryGraduation() {
         return queryGraduationService.execute();
+    }
+
+    @PatchMapping("/graduation")
+    public void updateGraduation(@RequestBody @Valid UpdateGraduationRequest request) {
+        updateGraduationService.execute(request);
     }
 }
