225660ca17e52ce56ed30d63953a454a76b5b4b1
leeseojune <sung07288346@gmail.com>
2021-11-05 01:36:44 +0900

    📑 :: ConverterPropertiesCreator

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/pdf/ConverterPropertiesCreator.java
@@ -0,0 +1,39 @@
+package kr.hs.entrydsm.rollsroyce.global.utils.pdf;
+
+import java.io.IOException;
+
+import com.itextpdf.html2pdf.ConverterProperties;
+import com.itextpdf.html2pdf.resolver.font.DefaultFontProvider;
+import com.itextpdf.io.font.FontProgram;
+import com.itextpdf.io.font.FontProgramFactory;
+import com.itextpdf.layout.font.FontProvider;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import static kr.hs.entrydsm.rollsroyce.global.config.Font.fonts;
+
+@Component
+public class ConverterPropertiesCreator {
+
+	@Value("${munchkin.font.path}")
+	private String fontPath;
+
+	public ConverterProperties createConverterProperties() {
+		ConverterProperties properties = new ConverterProperties();
+		FontProvider fontProvider = new DefaultFontProvider(false, false, false);
+
+		fonts.forEach(font -> {
+			try {
+				FontProgram fontProgram = FontProgramFactory.createFont(fontPath + font);
+				fontProvider.addFont(fontProgram);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+
+		properties.setFontProvider(fontProvider);
+		return properties;
+	}
+
+}
