b03d4791d7b0bb15262654b9b57196faa0ac90df
leeseojune <sung07288346@gmail.com>
2021-11-05 01:32:49 +0900

    📑 :: EducationalStatus

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/pdf/EducationalStatus.java
@@ -0,0 +1,9 @@
+package kr.hs.entrydsm.rollsroyce.global.utils.pdf;
+
+public class EducationalStatus {
+
+	public static final String GRADUATE = "GRADUATE";
+	public static final String PROSPECTIVE_GRADUATE = "PROSPECTIVE_GRADUATE";
+	public static final String QUALIFICATION_EXAM = "QUALIFICATION_EXAM";
+
+}
