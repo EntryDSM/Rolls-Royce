805137d37da124af93cf141aaded3f7fbf1e019f
김범진 <sdpthf@naver.com>
2021-11-04 17:29:32 +0900

    ⚡️ :: 검정고시 정보 입력 컨트롤러

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/ScoreController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/presentation/ScoreController.java
@@ -1,11 +1,13 @@
 package kr.hs.entrydsm.rollsroyce.domain.score.presentation;
 
 import kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.request.UpdateGraduationRequest;
+import kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.request.UpdateQualificationRequest;
 import kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.response.QueryGraduationResponse;
 import kr.hs.entrydsm.rollsroyce.domain.score.presentation.dto.response.QueryQualificationResponse;
 import kr.hs.entrydsm.rollsroyce.domain.score.service.QueryGraduationService;
 import kr.hs.entrydsm.rollsroyce.domain.score.service.UpdateGraduationService;
 import kr.hs.entrydsm.rollsroyce.domain.score.service.QueryQualificationService;
+import kr.hs.entrydsm.rollsroyce.domain.score.service.UpdateQualificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,7 @@ public class ScoreController {
     private final QueryGraduationService queryGraduationService;
     private final UpdateGraduationService updateGraduationService;
     private final QueryQualificationService queryQualificationService;
+    private final UpdateQualificationService updateQualificationService;
 
     @GetMapping("/graduation")
     public QueryGraduationResponse queryGraduation() {
@@ -34,4 +37,10 @@ public class ScoreController {
     public QueryQualificationResponse queryQualification() {
         return queryQualificationService.execute();
     }
+
+    @PatchMapping("/qualification")
+    public void updateQualification(@RequestBody @Valid UpdateQualificationRequest request) {
+        updateQualificationService.execute(request);
+    }
+
 }
