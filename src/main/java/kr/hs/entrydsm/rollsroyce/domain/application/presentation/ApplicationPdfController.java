cdc8b69dda5e596757d9248eba7830c939ea9a62
leeseojune <sung07288346@gmail.com>
2021-11-05 01:51:33 +0900

    📑 :: ApplicationPdfController

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/presentation/ApplicationPdfController.java
@@ -0,0 +1,40 @@
+package kr.hs.entrydsm.rollsroyce.domain.application.presentation;
+
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.http.HttpServletResponse;
+
+import kr.hs.entrydsm.rollsroyce.global.utils.pdf.ApplicationPdfService;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/pdf")
+@RequiredArgsConstructor
+public class ApplicationPdfController {
+
+	private static final String FILE_NAME = "대덕소프트웨어마이스터고등학교_입학원서";
+	private final ApplicationPdfService applicationPdfService;
+
+	@GetMapping(value = "/preview", produces = MediaType.APPLICATION_PDF_VALUE)
+	public byte[] getPreviewApplicationPdf() {
+		return applicationPdfService.getPreviewApplicationPdf();
+	}
+
+	@GetMapping(value = "/final", produces = MediaType.APPLICATION_PDF_VALUE)
+	public byte[] getFinalApplicationPdf(HttpServletResponse response) {
+		response.setHeader("Content-Disposition", String.format("attachment; filename=\"%s.pdf\"", encodeFileName()));
+		return applicationPdfService.getFinalApplicationPdf();
+	}
+
+	private String encodeFileName() {
+		return new String(ApplicationPdfController.FILE_NAME.getBytes(StandardCharsets.UTF_8),
+				StandardCharsets.ISO_8859_1);
+	}
+
+}
