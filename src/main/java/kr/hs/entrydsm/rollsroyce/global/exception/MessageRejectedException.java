a266db7d6964cf717765ce03cbc2e31449f93423
김범진 <sdpthf@naver.com>
2021-11-11 14:29:02 +0900

    📑 :: MessageRejectedException

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/exception/MessageRejectedException.java
@@ -0,0 +1,15 @@
+package kr.hs.entrydsm.rollsroyce.global.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class MessageRejectedException extends RollsException {
+
+    public static RollsException EXCEPTION =
+            new MessageRejectedException();
+
+    private MessageRejectedException() {
+        super(ErrorCode.MESSAGE_REJECTED);
+    }
+
+}
