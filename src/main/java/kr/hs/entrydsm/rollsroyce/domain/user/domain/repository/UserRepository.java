711904b8fdf475f651e32a36221ffdd7fef850a8
leeseojune <sung07288346@gmail.com>
2021-11-12 19:17:23 +0900

    ⚡ :: User dao findByApplicationTypeAndIsDaejeon

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
@@ -1,13 +1,15 @@
 package kr.hs.entrydsm.rollsroyce.domain.user.domain.repository;
 
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
+
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     List<User> findAllByStatusIsSubmittedTrue();
+    List<User> findByApplicationTypeAndIsDaejeon(ApplicationType applicationType, boolean isDaejeon);
 }
