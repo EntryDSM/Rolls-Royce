d403ef0ce65940bd8d03043648eca904327f057b
leeseojune <sung07288346@gmail.com>
2021-11-12 19:15:44 +0900

    📑 :: RouteResponse dto

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/openfeign/apis/dto/response/RouteResponse.java
@@ -0,0 +1,28 @@
+package kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis.dto.response;
+
+import java.util.List;
+
+import lombok.Getter;
+
+@Getter
+public class RouteResponse {
+
+	private String type;
+	private List<Feature> featureList;
+
+	@Getter
+	public static class Feature {
+		private String type;
+		private Properties properties;
+
+		@Getter
+		public static class Properties {
+			private int totalDistance;
+			private int totalTime;
+			private int totalFare;
+			private int taxiFare;
+		}
+
+	}
+
+}
