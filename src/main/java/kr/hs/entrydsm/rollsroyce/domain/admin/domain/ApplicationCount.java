77b8ad3098ce14c66e1f37bc77ae2d4f2750dd20
leeseojune <sung07288346@gmail.com>
2021-11-12 19:12:26 +0900

    📑 :: ApplicationCount entity

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/domain/ApplicationCount.java
@@ -0,0 +1,34 @@
+package kr.hs.entrydsm.rollsroyce.domain.admin.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "tbl_application_count")
+public class ApplicationCount {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Enumerated(EnumType.STRING)
+	private ApplicationType applicationType;
+
+	@Column(columnDefinition = "BIT(1)")
+	private boolean isDaejeon;
+
+	private int count;
+
+}
