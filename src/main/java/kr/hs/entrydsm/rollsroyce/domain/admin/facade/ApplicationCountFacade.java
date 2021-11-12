78d72429e6cedb0f87230a8e310e38c7f1e8d15a
leeseojune <sung07288346@gmail.com>
2021-11-12 19:14:40 +0900

    📑 :: ApplicationCountFacade

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/facade/ApplicationCountFacade.java
@@ -0,0 +1,22 @@
+package kr.hs.entrydsm.rollsroyce.domain.admin.facade;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.domain.repository.ApplicationCountRepository;
+import kr.hs.entrydsm.rollsroyce.domain.admin.exception.ApplicationCountNotFoundException;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ApplicationCountFacade {
+
+	private final ApplicationCountRepository applicationCountRepository;
+
+	public int countOfApplicationTypeAndIsDaejeon(ApplicationType applicationType, boolean isDaejeon) {
+		return applicationCountRepository.findByApplicationTypeAndIsDaejeon(applicationType, isDaejeon)
+				.orElseThrow(() -> ApplicationCountNotFoundException.EXCEPTION)
+				.getCount();
+	}
+
+}
