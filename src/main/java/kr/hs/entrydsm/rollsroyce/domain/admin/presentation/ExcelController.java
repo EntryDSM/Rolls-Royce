c5340010defc97fa689957af4e1f3659b484e9c5
leeseojune <sung07288346@gmail.com>
2021-11-12 19:20:09 +0900

    ⚡ :: /admin/excel/admission-ticket

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/ExcelController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/ExcelController.java
@@ -1,12 +1,14 @@
 package kr.hs.entrydsm.rollsroyce.domain.admin.presentation;
 
+import javax.servlet.http.HttpServletResponse;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.AdmissionTicketExcelService;
 import kr.hs.entrydsm.rollsroyce.domain.admin.service.ApplicantsExcelService;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @RequestMapping("/admin/excel")
@@ -14,10 +16,16 @@ import javax.servlet.http.HttpServletResponse;
 public class ExcelController {
 
     private final ApplicantsExcelService applicantsExcelService;
+    private final AdmissionTicketExcelService admissionTicketExcelService;
 
-    @GetMapping( "/applicants")
+    @GetMapping("/applicants")
     public void createApplicantInformation(HttpServletResponse response) {
         applicantsExcelService.execute(response);
     }
+
+	@GetMapping("/admission-ticket")
+	public void createAdmissionTicket(HttpServletResponse response) {
+		admissionTicketExcelService.execute(response);
+	}
 
 }
