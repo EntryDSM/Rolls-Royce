ff45b281c5e3411ee50df56a97d9c88759b59104
leeseojune <sung07288346@gmail.com>
2021-11-12 19:15:23 +0900

    📑 :: Feign 설정

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/openfeign/apis/FeignClientErrorDecoder.java
@@ -0,0 +1,22 @@
+package kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import kr.hs.entrydsm.rollsroyce.global.exception.RequestFailToOtherServerException;
+import lombok.extern.slf4j.Slf4j;
+
+import static feign.FeignException.errorStatus;
+
+@Slf4j
+public class FeignClientErrorDecoder implements ErrorDecoder {
+
+	@Override
+	public Exception decode(String methodKey, Response response) {
+
+		if(response.status() >= 400)
+			throw RequestFailToOtherServerException.EXCEPTION;
+
+		return errorStatus(methodKey, response);
+	}
+
+}
---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/openfeign/apis/FeignConfiguration.java
@@ -0,0 +1,28 @@
+package kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis;
+
+import feign.Logger;
+import feign.codec.ErrorDecoder;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@EnableFeignClients(basePackages = "kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis")
+@Import(FeignClientErrorDecoder.class)
+@Configuration
+public class FeignConfiguration {
+
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.BASIC;
+	}
+
+	@Bean
+	@ConditionalOnMissingBean(value = ErrorDecoder.class)
+	public FeignClientErrorDecoder commonFeignErrorDecoder() {
+		return new FeignClientErrorDecoder();
+	}
+
+}
