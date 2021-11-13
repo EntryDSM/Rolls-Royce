212cad21205a8b4e9c561d62ba6a8ef469429d01
leeseojune <sung07288346@gmail.com>
2021-11-13 21:21:37 +0900

    ⚡ :: /admin/statics/count

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/AdminController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/AdminController.java
@@ -1,15 +1,26 @@
 package kr.hs.entrydsm.rollsroyce.domain.admin.presentation;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.CheckPasswordRequest;
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.GetApplicantsRequest;
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.ApplicantsResponse;
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.*;
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.StaticsCountResponse;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.DeleteAllTablesService;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.GetApplicantsService;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.QueryStaticsCountService;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/admin")
@@ -18,6 +29,7 @@ public class AdminController {
 
     private final DeleteAllTablesService deleteAllTablesService;
     private final GetApplicantsService getApplicantsService;
+    private final QueryStaticsCountService queryStaticsCountService;
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/data")
@@ -25,9 +37,14 @@ public class AdminController {
         deleteAllTablesService.execute(request.getPassword());
     }
 
-    @GetMapping( "/applicants")
+    @GetMapping("/applicants")
     public ApplicantsResponse getApplicants(Pageable page, GetApplicantsRequest getApplicantsRequest) {
         return getApplicantsService.execute(page, getApplicantsRequest);
     }
+
+    @GetMapping("/statics/count")
+	public List<StaticsCountResponse> queryStaticsCount() {
+    	return queryStaticsCountService.execute();
+	}
 
 }
