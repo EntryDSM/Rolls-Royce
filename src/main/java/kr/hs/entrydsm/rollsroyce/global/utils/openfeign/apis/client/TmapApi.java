076ad3d78d363b926b28b9016109f194dc2e7a97
leeseojune <sung07288346@gmail.com>
2021-11-13 21:14:28 +0900

    🐛 :: OpenFeign QueryString

--- src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/openfeign/apis/client/TmapApi.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/openfeign/apis/client/TmapApi.java
@@ -1,15 +1,13 @@
 package kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis.client;
 
-import java.util.Map;
-
 import feign.Headers;
-import feign.Param;
 import kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis.dto.request.RouteRequest;
 import kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis.dto.response.CoordinateResponse;
 import kr.hs.entrydsm.rollsroyce.global.utils.openfeign.apis.dto.response.RouteResponse;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -24,12 +22,11 @@ public interface TmapApi {
 	})
 	RouteResponse routeGuidance(@RequestHeader("appKey") String key, @RequestBody RouteRequest request);
 
-	@GetMapping("/geo/postcode?version=1&addressFlag=F00&format=json&"
-			+ "appKey={key}&addr={addr}")
+	@GetMapping("/geo/postcode?addr={address}&version=1&addressFlag=F00&format=json")
 	@Headers({
 			"Accept-Language:ko",
 			"Content-Type:application/x-www-form-urlencoded;charset=UTF-8",
 	})
-	CoordinateResponse getCoordinate(@Param("queryParams") Map<String, String> queryParams);
+	CoordinateResponse getCoordinate(@RequestHeader("appKey") String key, @PathVariable("address") String addr);
 
 }
