a12a96db7a9a8e2350f1155a4257dd700bede3ee
김범진 <sdpthf@naver.com>
2021-11-14 23:38:57 +0900

    🐛 :: 이메일 전송 안됨

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/SendAuthCodeService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/SendAuthCodeService.java
@@ -4,12 +4,9 @@ import kr.hs.entrydsm.rollsroyce.domain.user.domain.AuthCode;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.AuthCodeLimit;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.AuthCodeLimitRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.AuthCodeRepository;
-import kr.hs.entrydsm.rollsroyce.domain.user.exception.AuthCodeRequestOverLimitException;
-import kr.hs.entrydsm.rollsroyce.domain.user.exception.UserAlreadyExistsException;
 import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserAuthCodeFacade;
 import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.SendEmailRequest;
-import kr.hs.entrydsm.rollsroyce.global.exception.MessageRejectedException;
 import kr.hs.entrydsm.rollsroyce.global.utils.ses.SESUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,7 +21,6 @@ import java.util.Optional;
 public class SendAuthCodeService {
 
     private static final long authCodeTTL = 3 * 60;
-    private static final long authCodeLimit = 5;
 
     private final SESUtil sesUtil;
     private final UserFacade userFacade;
@@ -40,19 +36,10 @@ public class SendAuthCodeService {
         Map<String, String> params = new HashMap<>();
         params.put("code", code);
 
-        if(isOverLimit(email)) {
-            throw AuthCodeRequestOverLimitException.EXCEPTION;
-        }
-
-        if(userFacade.isAlreadyExists(email)) {
-            throw UserAlreadyExistsException.EXCEPTION;
-        }
-
-        if(sesUtil.sendMessage(email, "RollsRoyceEmailTemplate", params)) {
-            throw MessageRejectedException.EXCEPTION;
-        }
-
         authCodeRepository.findById(email)
+                .filter(s -> isOverLimit(email))
+                .filter(s -> userFacade.isAlreadyExists(email))
+                .filter(s -> sesUtil.sendMessage(email, "RollsRoyceEmailTemplate", params))
                 .map(authCode -> authCode.updateAuthCode(code, authCodeTTL))
                 .orElseGet(() -> authCodeRepository.save(AuthCode.builder()
                         .email(email)
@@ -64,15 +51,16 @@ public class SendAuthCodeService {
     }
 
     private boolean isOverLimit(String email) {
-        return !authCodeLimitRepository.findById(email)
-                .filter(limit -> limit.getCount() < authCodeLimit)
+        authCodeLimitRepository.findById(email)
+                .filter(limit -> authCodeFacade.checkCount(limit.getCount()))
                 .map(authCodeLimit -> authCodeLimit.updateAuthCode(authCodeTTL))
                 .or(() -> Optional.of(authCodeLimitRepository.save(AuthCodeLimit.builder()
                         .email(email)
                         .count(1)
                         .ttl(authCodeTTL)
-                        .build())))
-                .isPresent();
+                        .build())));
+
+        return true;
     }
 
 }
