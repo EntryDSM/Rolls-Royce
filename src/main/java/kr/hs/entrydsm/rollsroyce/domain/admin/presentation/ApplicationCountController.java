881fe1a094d65f7756b22e0ab47e24f57891a8f5
이승윤 <yyuunn17@naver.com>
2021-12-03 15:13:18 +0900

    ♻️ :: controller di 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/ApplicationCountController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/ApplicationCountController.java
@@ -16,9 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @RequiredArgsConstructor
 @RequestMapping("/admin/application-count")
+@RestController
 public class ApplicationCountController {
 
 	private final QueryApplicationCountService queryApplicationCountService;
