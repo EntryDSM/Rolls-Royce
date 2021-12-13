0df2ec8f17d91408b0245a42f8d390378d344021
leeseojune <sung07288346@gmail.com>
2021-12-13 16:02:48 +0900

    📑 :: Pdf Configruation

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/config/PdfConfig.java
@@ -0,0 +1,23 @@
+package kr.hs.entrydsm.rollsroyce.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class PdfConfig {
+
+    @Bean
+    public TemplateEngine templateEngine() {
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        return templateEngine;
+    }
+}
