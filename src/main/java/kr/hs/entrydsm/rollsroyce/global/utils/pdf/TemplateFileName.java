240820dfaa5af185a9d1a3f2659532745d98981b
leeseojune <sung07288346@gmail.com>
2021-12-13 12:03:16 +0900

    🐛 :: remove prefix

--- src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/pdf/TemplateFileName.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/utils/pdf/TemplateFileName.java
@@ -2,10 +2,10 @@ package kr.hs.entrydsm.rollsroyce.global.utils.pdf;
 
 public class TemplateFileName {
 
-	public static final String APPLICATION_FOR_ADMISSION = "/templates/application_for_admission";
-	public static final String INTRODUCTION = "/templates/introduction";
-	public static final String NON_SMOKING = "/templates/nonsmoking";
-	public static final String RECOMMENDATION = "/templates/recommendation";
-	public static final String ADMISSION_AGREEMENT = "/templates/admission_agreement";
+	public static final String APPLICATION_FOR_ADMISSION = "application_for_admission";
+	public static final String INTRODUCTION = "introduction";
+	public static final String NON_SMOKING = "nonsmoking";
+	public static final String RECOMMENDATION = "recommendation";
+	public static final String ADMISSION_AGREEMENT = "admission_agreement";
 
 }
