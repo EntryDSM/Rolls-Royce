1a8870a9a8c5eaca98856a63134ba6f3a0e497bd
Jeong-DaeHyun <eogus_2513@dsm.hs.kr>
2021-12-16 14:55:06 +0900

    ⚡️ :: ChangeStudyPlanService Transactional 어노테이션 추가

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/service/ChangeStudyPlanService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/service/ChangeStudyPlanService.java
@@ -5,6 +5,7 @@ import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
 import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -12,6 +13,7 @@ public class ChangeStudyPlanService {
 
     private final UserFacade userFacade;
 
+    @Transactional
     public void execute(ChangeStudyPlanRequest request) {
         User user = userFacade.getCurrentUser();
         user.updateStudyPlan(request.getContent());
