d905b47a09b06427f82d08edd94ca026453f37bd
leeseojune <sung07288346@gmail.com>
2021-12-20 18:33:25 +0900

    ♻ :: Application Facade

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/facade/ApplicationFacade.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/facade/ApplicationFacade.java
@@ -1,9 +1,11 @@
 package kr.hs.entrydsm.rollsroyce.domain.application.facade;
 
+import kr.hs.entrydsm.rollsroyce.domain.application.domain.Application;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.Graduation;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.Qualification;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.repository.GraduationRepository;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.repository.QualificationRepository;
+import kr.hs.entrydsm.rollsroyce.domain.school.domain.School;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.EducationalStatus;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.ApplicationNotFoundException;
@@ -17,58 +19,77 @@ import java.time.LocalDate;
 @RequiredArgsConstructor
 public class ApplicationFacade {
 
-	private final GraduationRepository graduationRepository;
-	private final QualificationRepository qualificationRepository;
+    private final GraduationRepository graduationRepository;
+    private final QualificationRepository qualificationRepository;
 
-	public void updateQualification(User user, LocalDate qualifiedAt) {
-		graduationRepository.findById(user.getReceiptCode())
-				.ifPresent(graduationRepository::delete);
-		if (qualificationRepository.findById(user.getReceiptCode()).isPresent())
-			qualificationRepository.findById(user.getReceiptCode())
-					.ifPresent(qualification -> qualification.updateQualifiedAt(qualifiedAt));
-		else
-			qualificationRepository.save(
-					new Qualification(user, qualifiedAt)
-			);
-	}
+    public void changeInformation(Long receiptCode, School school, String studentNumber, String schoolTel) {
+        getGraduation(receiptCode)
+                .changeGraduationInformation(school, studentNumber, schoolTel);
+    }
 
-	public void updateGraduation(User user, LocalDate graduatedAt, String educationalStatus) {
-		qualificationRepository.findById(user.getReceiptCode())
-				.ifPresent(qualificationRepository::delete);
-		if (graduationRepository.findById(user.getReceiptCode()).isPresent())
-			graduationRepository.findById(user.getReceiptCode())
-					.ifPresent(graduation -> graduation.updateInformation(graduatedAt,
-							EnumUtil.getEnum(EducationalStatus.class, educationalStatus)));
-		else
-			graduationRepository.save(
-					new Graduation(user, graduatedAt,
-							EnumUtil.getEnum(EducationalStatus.class, educationalStatus))
-			);
-	}
+    public void updateInformation(User user, LocalDate graduatedAt, String educationalStatus) {
+        if (EducationalStatus.QUALIFICATION_EXAM.name().equals(educationalStatus)) {
+            updateQualification(user, graduatedAt);
+        } else {
+            updateGraduation(user, graduatedAt, educationalStatus);
+        }
+    }
 
-	public Graduation getGraduation(Long receiptCode) {
-		return graduationRepository.findById(receiptCode)
-				.orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
-	}
+    public void updateQualification(User user, LocalDate qualifiedAt) {
+        graduationRepository.findById(user.getReceiptCode())
+                .ifPresent(graduationRepository::delete);
+        if (qualificationRepository.findById(user.getReceiptCode()).isPresent())
+            qualificationRepository.findById(user.getReceiptCode())
+                    .ifPresent(qualification -> qualification.updateQualifiedAt(qualifiedAt));
+        else
+            qualificationRepository.save(
+                    new Qualification(user, qualifiedAt)
+            );
+    }
 
-	public Qualification getQualification(Long receiptCode) {
-		return qualificationRepository.findById(receiptCode)
-				.orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
-	}
+    public void updateGraduation(User user, LocalDate graduatedAt, String educationalStatus) {
+        qualificationRepository.findById(user.getReceiptCode())
+                .ifPresent(qualificationRepository::delete);
+        if (graduationRepository.findById(user.getReceiptCode()).isPresent())
+            graduationRepository.findById(user.getReceiptCode())
+                    .ifPresent(graduation -> graduation.updateInformation(graduatedAt,
+                            EnumUtil.getEnum(EducationalStatus.class, educationalStatus)));
+        else
+            graduationRepository.save(
+                    new Graduation(user, graduatedAt,
+                            EnumUtil.getEnum(EducationalStatus.class, educationalStatus))
+            );
+    }
 
-	public Graduation getGraduationOrNull(Long receiptCode) {
-		return graduationRepository.findById(receiptCode)
-				.orElse(null);
-	}
+    public Application getApplication(Long receiptCode, EducationalStatus educationalStatus) {
+        if(EducationalStatus.QUALIFICATION_EXAM.equals(educationalStatus))
+            return getQualification(receiptCode);
+        else return getGraduation(receiptCode);
+    }
 
-	public Qualification getQualificationOrNull(Long receiptCode) {
-		return qualificationRepository.findById(receiptCode)
-				.orElse(null);
-	}
+    public Graduation getGraduation(Long receiptCode) {
+        return graduationRepository.findById(receiptCode)
+                .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
+    }
 
-	public void deleteAll() {
-		graduationRepository.deleteAll();
-		qualificationRepository.deleteAll();
-	}
+    public Qualification getQualification(Long receiptCode) {
+        return qualificationRepository.findById(receiptCode)
+                .orElseThrow(() -> ApplicationNotFoundException.EXCEPTION);
+    }
+
+    public Graduation getGraduationOrNull(Long receiptCode) {
+        return graduationRepository.findById(receiptCode)
+                .orElse(null);
+    }
+
+    public Qualification getQualificationOrNull(Long receiptCode) {
+        return qualificationRepository.findById(receiptCode)
+                .orElse(null);
+    }
+
+    public void deleteAll() {
+        graduationRepository.deleteAll();
+        qualificationRepository.deleteAll();
+    }
 
 }
