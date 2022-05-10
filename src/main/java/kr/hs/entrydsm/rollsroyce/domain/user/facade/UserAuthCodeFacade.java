8cabd8db1dede6ad973bb6b72530920f7e3e6aaf
정대현 <eogus_2513@dsm.hs.kr>
2022-05-11 01:25:37 +0900

    :recycle: :: (#338) 중복된 코드 해결

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/facade/UserAuthCodeFacade.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/facade/UserAuthCodeFacade.java
@@ -5,17 +5,22 @@ import kr.hs.entrydsm.rollsroyce.domain.user.domain.AuthCodeLimit;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.AuthCodeLimitRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.AuthCodeRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserRepository;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.Action;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.AuthCodeAlreadyVerifiedException;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.AuthCodeRequestOverLimitException;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.InvalidAuthCodeException;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.UnVerifiedAuthCodeException;
 import kr.hs.entrydsm.rollsroyce.domain.user.exception.UserNotFoundException;
+import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.SendEmailRequest;
+import kr.hs.entrydsm.rollsroyce.global.exception.MessageRejectedException;
+import kr.hs.entrydsm.rollsroyce.global.utils.ses.SESUtil;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -25,6 +30,7 @@ public class UserAuthCodeFacade {
     private final AuthCodeRepository authCodeRepository;
     private final AuthCodeLimitRepository authCodeLimitRepository;
     private final UserRepository userRepository;
+    private final SESUtil sesUtil;
 
     @Value("${auth.code.limit}")
     private long authCodeLimit;
@@ -80,15 +86,39 @@ public class UserAuthCodeFacade {
                         .build()));
     }
 
-    public boolean checkFilter(String email) {
+    public void checkFilter(String email) {
         isOverLimit(email);
         if (!(userFacade.isAlreadyExists(email) && !isVerified(email))) {
             throw AuthCodeAlreadyVerifiedException.EXCEPTION;
         }
-        return true;
     }
 
-    public boolean checkPasswordEmailFilter(String email) {
+    public void verifySendEmail(SendEmailRequest request, String templateName, Action action) {
+        String email = request.getEmail();
+        String code = getRandomCode();
+
+        Map<String, String> params = new HashMap<>();
+        params.put("code", code);
+
+        AuthCode authCode = authCodeRepository.findById(email)
+                .orElseGet(() -> buildAuthCode(email, code, authCodeLimitTTL));
+
+        if (Action.PASSWORD.equals(action)) {
+            checkPasswordEmailFilter(email);
+        } else {
+            checkFilter(request.getEmail());
+        }
+
+        if (sesUtil.sendMessage(email, templateName, params)) {
+            throw MessageRejectedException.EXCEPTION;
+        }
+
+        authCode.updateAuthCode(code, authCodeLimitTTL * 1000);
+
+        authCodeRepository.save(authCode);
+    }
+
+    public void checkPasswordEmailFilter(String email) {
         isOverLimit(email);
         if (userRepository.findByEmail(email).isEmpty()) {
             throw UserNotFoundException.EXCEPTION;
@@ -96,16 +126,15 @@ public class UserAuthCodeFacade {
         if (isVerified(email)) {
             throw UnVerifiedAuthCodeException.EXCEPTION;
         }
-        return true;
     }
 
-    public Optional<AuthCode> buildAuthCode(String email, String code, Long authCodeTTL) {
-        return Optional.of(authCodeRepository.save(AuthCode.builder()
+    public AuthCode buildAuthCode(String email, String code, Long authCodeTTL) {
+        return authCodeRepository.save(AuthCode.builder()
                 .email(email)
                 .code(code)
                 .isVerified(false)
                 .ttl(authCodeTTL * 1000)
-                .build()));
+                .build());
     }
 
     public boolean isVerified(String email) {
--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/SendEmailAuthCodeService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/SendEmailAuthCodeService.java
@@ -1,42 +1,23 @@
 package kr.hs.entrydsm.rollsroyce.domain.user.service;
 
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.AuthCodeRepository;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.Action;
 import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserAuthCodeFacade;
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.SendEmailRequest;
-import kr.hs.entrydsm.rollsroyce.global.utils.ses.SESUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class SendEmailAuthCodeService {
 
-    private final SESUtil sesUtil;
     private final UserAuthCodeFacade authCodeFacade;
-    private final AuthCodeRepository authCodeRepository;
-
-    @Value("${auth.code.exp}")
-    private long authCodeTTL;
 
     @Transactional
     public void execute(SendEmailRequest request, String templateName) {
-        String email = request.getEmail();
-        String code = authCodeFacade.getRandomCode();
 
-        Map<String, String> params = new HashMap<>();
-        params.put("code", code);
+        authCodeFacade.verifySendEmail(request, templateName, Action.EMAIL);
 
-        authCodeRepository.findById(email)
-                .or(() -> authCodeFacade.buildAuthCode(email, code, authCodeTTL))
-                .filter(s -> authCodeFacade.checkFilter(email))
-                .filter(s -> sesUtil.sendMessage(email, templateName, params))
-                .map(authCode -> authCode.updateAuthCode(code, authCodeTTL * 1000))
-                .map(authCodeRepository::save);
     }
 
 }
