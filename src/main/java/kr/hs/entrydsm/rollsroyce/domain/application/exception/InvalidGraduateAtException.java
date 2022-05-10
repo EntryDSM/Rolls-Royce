c4826248cbeaca68e2b139a429ab19656fdc1e04
KimBeomJin <sdpthf@naver.com>
2022-05-11 01:24:24 +0900

    📑 :: InvalidGraduateAtException

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/exception/InvalidGraduateAtException.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.rollsroyce.domain.application.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class InvalidGraduateAtException extends RollsException  {
+
+    public static final RollsException EXCEPTION = new InvalidGraduateAtException();
+
+    private InvalidGraduateAtException() {
+        super(ErrorCode.INVALID_GRADUATE_AT);
+    }
+}
