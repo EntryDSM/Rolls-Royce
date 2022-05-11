e0e8d50b316c798875de8e89b0bc7c57bcd1b4c5
이승윤 <yyuunn17@naver.com>
2022-05-12 01:16:18 +0900

    ⚡️ :: (#355) 테이블 삭제할 때는 password 확인하지 않기

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/DeleteAllTablesService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/DeleteAllTablesService.java
@@ -1,10 +1,7 @@
 package kr.hs.entrydsm.rollsroyce.domain.admin.service;
 
-import java.time.LocalDateTime;
-
 import kr.hs.entrydsm.rollsroyce.domain.admin.domain.Admin;
 import kr.hs.entrydsm.rollsroyce.domain.admin.exception.ApplicationPeriodNotOverException;
-import kr.hs.entrydsm.rollsroyce.domain.admin.exception.PasswordNotValidException;
 import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminFacade;
 import kr.hs.entrydsm.rollsroyce.domain.application.facade.ApplicationFacade;
 import kr.hs.entrydsm.rollsroyce.domain.schedule.domain.types.Type;
@@ -15,9 +12,9 @@ import kr.hs.entrydsm.rollsroyce.domain.score.domain.repository.ScoreRepository;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.repository.StatusRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Service
@@ -33,19 +30,13 @@ public class DeleteAllTablesService {
 
     private final AdminFacade adminFacade;
 
-    private final PasswordEncoder passwordEncoder;
-
-    public void execute(String password) {
+    public void execute() {
         if (!scheduleFacade.getScheduleByType(Type.SECOND_ANNOUNCEMENT)
 				.isAfter(LocalDateTime.now())) {
             throw ApplicationPeriodNotOverException.EXCEPTION;
         }
 
         Admin admin = adminFacade.getRootAdmin();
-
-        if (!passwordEncoder.matches(password, admin.getPassword())) {
-            throw PasswordNotValidException.EXCEPTION;
-        }
 
         scoreRepository.deleteAll();
         graduationCaseRepository.deleteAll();
