b75f5294bda79c85f2750ec364b11d16e66debf4
이승윤 <yyuunn17@naver.com>
2022-05-12 01:16:40 +0900

    ⚡️ :: (#355) 비밀번호 확인 API

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/CheckPasswordService.java
@@ -0,0 +1,29 @@
+package kr.hs.entrydsm.rollsroyce.domain.admin.service;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.domain.Admin;
+import kr.hs.entrydsm.rollsroyce.domain.admin.domain.repository.AdminRepository;
+import kr.hs.entrydsm.rollsroyce.domain.admin.exception.PasswordNotValidException;
+import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminFacade;
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.CheckPasswordRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CheckPasswordService {
+
+    private final AdminRepository adminRepository;
+
+    private final AdminFacade adminFacade;
+    private final PasswordEncoder passwordEncoder;
+
+    public void execute(CheckPasswordRequest request) {
+        Admin admin = adminFacade.getAdmin();
+
+        if (!passwordEncoder.matches(request.getPassword(), admin.getPassword())) {
+            throw PasswordNotValidException.EXCEPTION;
+        }
+    }
+
+}
