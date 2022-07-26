21baeeb87d03c579c897cf06cdc4bc6fb5bcc9e6
KimBeomJin <sdpthf@naver.com>
2022-07-26 20:42:53 +0900

    ♻️ :: 깔끔하게 정리

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/ApplicationCase.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/ApplicationCase.java
@@ -1,71 +1,73 @@
 package kr.hs.entrydsm.rollsroyce.domain.score.domain;
 
-import java.math.BigDecimal;
-
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
-
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.EducationalStatus;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.math.BigDecimal;
 
 @NoArgsConstructor
 @MappedSuperclass
 public abstract class ApplicationCase {
 
-	protected static final BigDecimal COMMON_GRADE_RATE = BigDecimal.valueOf(1.75);
+    protected static final BigDecimal COMMON_GRADE_RATE = BigDecimal.valueOf(1.75);
 
-	protected static final int MAX_VOLUNTEER_TIME = 12;
-	protected static final int MIN_VOLUNTEER_TIME = 1;
+    protected static final int MAX_VOLUNTEER_TIME = 12;
+    protected static final int MIN_VOLUNTEER_TIME = 1;
 
-	protected static final int MAX_VOLUNTEER_SCORE = 15;
-	protected static final int MIN_VOLUNTEER_SCORE = 3;
+    protected static final int MAX_VOLUNTEER_SCORE = 15;
+    protected static final int MIN_VOLUNTEER_SCORE = 3;
 
-	protected static final int MAX_ATTENDANCE_SCORE = 15;
+    protected static final int MAX_ATTENDANCE_SCORE = 15;
 
-	@Id
-	protected Long receiptCode;
-	protected boolean isDaejeon;
-	protected ApplicationType applicationType;
-	protected EducationalStatus educationalStatus;
+    @Id
+    protected Long receiptCode;
+    protected boolean isDaejeon;
+    protected ApplicationType applicationType;
+    protected EducationalStatus educationalStatus;
 
-	protected ApplicationCase(Long receiptCode,
-							  boolean isDaejeon,
-							  ApplicationType applicationType,
-							  EducationalStatus educationalStatus) {
-		this.receiptCode = receiptCode;
-		this.isDaejeon = isDaejeon;
-		this.applicationType = applicationType;
-		this.educationalStatus = educationalStatus;
-	}
+    protected ApplicationCase(Long receiptCode,
+                              boolean isDaejeon,
+                              ApplicationType applicationType,
+                              EducationalStatus educationalStatus) {
+        this.receiptCode = receiptCode;
+        this.isDaejeon = isDaejeon;
+        this.applicationType = applicationType;
+        this.educationalStatus = educationalStatus;
+    }
 
-	abstract BigDecimal calculateVolunteerScore();
-	abstract Integer calculateAttendanceScore();
-	abstract BigDecimal[] calculateGradeScores();
-	abstract BigDecimal calculateTotalGradeScore();
+    abstract BigDecimal calculateVolunteerScore();
 
-	public boolean isDaejeon() {
-		return isDaejeon;
-	}
+    abstract Integer calculateAttendanceScore();
 
-	public boolean isMeister() {
-		return applicationType == ApplicationType.MEISTER;
-	}
+    abstract BigDecimal[] calculateGradeScores();
 
-	public boolean isCommon() {
-		return applicationType == ApplicationType.COMMON;
-	}
+    abstract BigDecimal calculateTotalGradeScore();
 
-	public boolean isQualificationExam() {
-		return educationalStatus == EducationalStatus.QUALIFICATION_EXAM;
-	}
+    public boolean isDaejeon() {
+        return isDaejeon;
+    }
 
-	public boolean isProspectiveGraduate() {
-		return educationalStatus == EducationalStatus.PROSPECTIVE_GRADUATE;
-	}
+    public boolean isMeister() {
+        return applicationType == ApplicationType.MEISTER;
+    }
 
-	public boolean isGraduated() {
-		return educationalStatus == EducationalStatus.GRADUATE;
-	}
+    public boolean isCommon() {
+        return applicationType == ApplicationType.COMMON;
+    }
+
+    public boolean isQualificationExam() {
+        return educationalStatus == EducationalStatus.QUALIFICATION_EXAM;
+    }
+
+    public boolean isProspectiveGraduate() {
+        return educationalStatus == EducationalStatus.PROSPECTIVE_GRADUATE;
+    }
+
+    public boolean isGraduated() {
+        return educationalStatus == EducationalStatus.GRADUATE;
+    }
 
 }
