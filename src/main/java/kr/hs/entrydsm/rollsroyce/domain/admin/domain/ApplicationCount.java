0db2b287953dbfa76acb27de0f68b29ee445ed95
이승윤 <yyuunn17@naver.com>
2022-08-07 20:02:29 +0900

    ♻️ :: admin domain 리팩토링

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/domain/ApplicationCount.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/domain/ApplicationCount.java
@@ -1,5 +1,10 @@
 package kr.hs.entrydsm.rollsroyce.domain.admin.domain;
 
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -8,38 +13,32 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity(name = "tbl_application_count")
 public class ApplicationCount {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Enumerated(EnumType.STRING)
-	private ApplicationType applicationType;
+    @Enumerated(EnumType.STRING)
+    private ApplicationType applicationType;
 
-	@Column(columnDefinition = "BIT(1)")
-	private boolean isDaejeon;
+    @Column(columnDefinition = "BIT(1)")
+    private boolean isDaejeon;
 
-	private int count;
+    private int count;
 
-	public ApplicationCount(ApplicationType applicationType, boolean isDaejeon, int count) {
-		this.applicationType = applicationType;
-		this.isDaejeon = isDaejeon;
-		this.count = count;
-	}
+    public ApplicationCount(ApplicationType applicationType, boolean isDaejeon, int count) {
+        this.applicationType = applicationType;
+        this.isDaejeon = isDaejeon;
+        this.count = count;
+    }
 
-	public ApplicationCount update(int count) {
-		this.count = count;
-		return this;
-	}
+    public ApplicationCount update(int count) {
+        this.count = count;
+        return this;
+    }
 
 }
