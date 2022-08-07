2a8b91d8843fdaa8eb3c4ede2c0f78ec85622efd
이승윤 <yyuunn17@naver.com>
2022-08-07 20:10:47 +0900

    ♻️ :: exception 리팩토링

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/exception/RequestFailToTmapServerException.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/exception/RequestFailToTmapServerException.java
@@ -5,7 +5,8 @@ import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
 
 public class RequestFailToTmapServerException extends RollsException {
 
-    public static final RollsException EXCEPTION = new RequestFailToTmapServerException();
+    public static final RollsException EXCEPTION =
+            new RequestFailToTmapServerException();
 
     private RequestFailToTmapServerException() {
         super(ErrorCode.REQUEST_FAIL_TO_TMAP_SERVER);
