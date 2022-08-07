4413811fab5f595959aa716264906a6c73edb337
이승윤 <yyuunn17@naver.com>
2022-08-07 19:21:54 +0900

    ♻️ :: LogProperties 사용

--- src/main/java/kr/hs/entrydsm/rollsroyce/global/security/logging/LogWriter.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/logging/LogWriter.java
@@ -1,5 +1,9 @@
 package kr.hs.entrydsm.rollsroyce.global.security.logging;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,80 +19,66 @@ import java.time.format.DateTimeFormatter;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
-
 @Component
 public class LogWriter {
 
-	@Value("${rolls-royce.logging.name}")
-	private String logFileName;
+    private final LogProperties logProperties;
 
-	@Value("${rolls-royce.logging.path}")
-	private String logPath;
+    private final File file;
+    private BufferedWriter writer;
 
-	@Value("${rolls-royce.logging.size}")
-	private int logSize;
+    public LogWriter(LogProperties logProperties) throws IOException {
+        this.logProperties = logProperties;
+        file = new File(logProperties.getPath(), logProperties.getName());
+        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true)));
+    }
 
-	private File file;
-	private BufferedWriter writer;
+    @Async
+    public void writeLog(String content) throws IOException {
+        writer.write(content + "\n");
+        writer.flush();
+        if (isLogOverSize()) {
+            createZipFile();
+        }
+    }
 
-	@Async
-	public void writeLog(String content) throws IOException {
-		writer.write(content + "\n");
-		writer.flush();
-		if (isLogOverSize()) {
-			createZipFile();
-		}
-	}
+    @PreDestroy
+    public void closeStream() throws IOException {
+        writer.close();
+    }
 
-	@PostConstruct
-	public void setup() throws IOException {
-		file = new File(logPath, logFileName);
-		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file, true)));
-	}
+    private boolean isLogOverSize() throws IOException {
+        double size = BigDecimal.valueOf(Files.size(file.toPath()))
+                .divide(BigDecimal.valueOf(1048576), 2, RoundingMode.HALF_UP)
+                .doubleValue();
+        return logProperties.getSize() < size;
+    }
 
-	@PreDestroy
-	public void closeStream() throws IOException {
-		writer.close();
-	}
+    private void createZipFile() throws IOException {
+        String now = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
+                .format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        File zipFile = new File(logProperties.getPath(), logProperties.getName() + "." + now + ".zip");
+        FileInputStream logInputStream = new FileInputStream(file);
+        FileOutputStream logOutputStream = new FileOutputStream(zipFile);
+        ZipOutputStream zipOutputStream = new ZipOutputStream(logOutputStream);
+        ZipEntry zipEntry = new ZipEntry(logProperties.getName());
+        zipOutputStream.putNextEntry(zipEntry);
 
-	private boolean isLogOverSize() throws IOException {
-		double size = BigDecimal.valueOf(Files.size(file.toPath()))
-				.divide(BigDecimal.valueOf(1048576), 2, RoundingMode.HALF_UP)
-				.doubleValue();
-		return logSize < size;
-	}
+        int length;
+        byte[] buffer = new byte[8196];
+        while ((length = logInputStream.read(buffer)) > 0) {
+            zipOutputStream.write(buffer, 0, length);
+        }
 
-	private void createZipFile() throws IOException {
-		String now = LocalDateTime.now(ZoneId.of("Asia/Seoul"))
-				.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
-		File zipFile = new File(logPath, logFileName + "." + now + ".zip");
-		FileInputStream logInputStream = new FileInputStream(file);
-		FileOutputStream logOutputStream = new FileOutputStream(zipFile);
-		ZipOutputStream zipOutputStream = new ZipOutputStream(logOutputStream);
-		ZipEntry zipEntry = new ZipEntry(logFileName);
-		zipOutputStream.putNextEntry(zipEntry);
+        zipOutputStream.closeEntry();
+        zipOutputStream.finish();
+        logInputStream.close();
+        zipOutputStream.close();
+        resetLogFile();
+    }
 
-		int length;
-		byte[] buffer = new byte[8196];
-		while((length = logInputStream.read(buffer)) > 0) {
-			zipOutputStream.write(buffer, 0, length);
-		}
-
-		zipOutputStream.closeEntry();
-		zipOutputStream.finish();
-		logInputStream.close();
-		zipOutputStream.close();
-		resetLogFile();
-	}
-
-	private void resetLogFile() throws IOException {
-		writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
-	}
+    private void resetLogFile() throws IOException {
+        writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file)));
+    }
 
 }
