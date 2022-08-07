d2101d23e88986f1dfc6fbe597a1fd0149ee9050
이승윤 <yyuunn17@naver.com>
2022-08-07 20:52:32 +0900

    ♻️ ::	RollsRoyceApplication 리팩토링

--- src/main/java/kr/hs/entrydsm/rollsroyce/RollsRoyceApplication.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/RollsRoyceApplication.java
@@ -4,13 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-		ThymeleafAutoConfiguration.class
-})
+@SpringBootApplication(exclude = {ThymeleafAutoConfiguration.class})
 public class RollsRoyceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RollsRoyceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RollsRoyceApplication.class, args);
+    }
 
 }
