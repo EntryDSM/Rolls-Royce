fe067c445c254b642ac8bf1dea0f8f53181ed259
이승윤 <yyuunn17@naver.com>
2022-08-07 19:19:27 +0900

    ♻️ :: security>auth 리팩토링

--- src/main/java/kr/hs/entrydsm/rollsroyce/global/security/auth/AuthDetails.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/auth/AuthDetails.java
@@ -1,53 +1,57 @@
 package kr.hs.entrydsm.rollsroyce.global.security.auth;
 
-import java.util.Collection;
-import java.util.Collections;
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Getter
 @AllArgsConstructor
 public class AuthDetails implements UserDetails {
 
-	private final String receiptCode;
+    private final String receiptCode;
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return Collections.emptyList();
-	}
+    private static final String ROLE_USER = "ROLE_USER";
 
-	@Override
-	public String getPassword() {
-		return null;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(
+                new SimpleGrantedAuthority(ROLE_USER)
+        );
+    }
 
-	@Override
-	public String getUsername() {
-		return receiptCode;
-	}
+    @Override
+    public String getPassword() {
+        return null;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @Override
+    public String getUsername() {
+        return receiptCode;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 
 }
