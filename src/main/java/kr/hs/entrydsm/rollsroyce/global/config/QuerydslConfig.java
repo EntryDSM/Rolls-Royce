14e5c0b0672d62905aa3a4e1d87f45bccf76230c
leeseojune53 <sung07288346@gmail.com>
2022-08-11 23:13:39 +0900

    📑 :: QuerydslConfig

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/config/QuerydslConfig.java
@@ -0,0 +1,21 @@
+package kr.hs.entrydsm.rollsroyce.global.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QuerydslConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
