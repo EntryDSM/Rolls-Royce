d01fc0aeddf0cf0a6004ea53bea1f186ca59f6d2
osangu <hhgnoon@naver.com>
2022-08-20 02:19:39 +0900

    ⚰️ :: 해당 API 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/ApplicationController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/ApplicationController.java
@@ -1,6 +1,5 @@
 package kr.hs.entrydsm.rollsroyce.domain.admin.presentation;
 
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.CancelApplicationSubmitService;
 import kr.hs.entrydsm.rollsroyce.domain.admin.service.UpdateIsPrintsArrivedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,14 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("admin.presentation.ApplicationController")
 public class ApplicationController {
 
-    private final CancelApplicationSubmitService cancelApplicationSubmitService;
     private final UpdateIsPrintsArrivedService updateIsPrintsArrivedService;
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PatchMapping("/submitted/{receipt-code}")
-    public void cancelApplicationSubmitStatus(@PathVariable("receipt-code") long receiptCode) {
-        cancelApplicationSubmitService.execute(receiptCode);
-    }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/prints-arrived/{receipt-code}")
