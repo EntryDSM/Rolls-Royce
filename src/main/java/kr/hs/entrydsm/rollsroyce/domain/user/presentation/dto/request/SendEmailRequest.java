5d17e61cb25d1abaa00cb88e73b6e45972eeddfe
이준서 <ljchan71@gmail.com>
2022-09-01 15:53:48 +0900

    ♻️ :: 취약점 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/dto/request/SendEmailRequest.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/dto/request/SendEmailRequest.java
@@ -1,11 +1,13 @@
 package kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request;
 
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,6 +15,7 @@ public class SendEmailRequest {
 
     @NotBlank(message = "email은 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Size(max = 100, message = "이메일이 너무 깁니다.")
     private String email;
 
 }
