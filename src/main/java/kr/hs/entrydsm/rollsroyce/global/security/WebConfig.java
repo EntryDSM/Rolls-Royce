8c0ee1fa4f011a1f8e63fc401a956483ec5d4a5c
정대현 <eogus_2513@dsm.hs.kr>
2022-09-07 16:20:41 +0900

    :recycle: :: origins 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/global/security/WebConfig.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/WebConfig.java
@@ -1,6 +1,7 @@
 package kr.hs.entrydsm.rollsroyce.global.security;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,12 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.origins}")
+    private String allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "DELETE", "PATCH", "PUT")
-                .allowedOrigins("http://localhost:3000", "http://localhost:3001",
-						"http://localhost:3002", "https://apply.entrydsm.hs.kr", "https://admin.entrydsm.hs.kr");
+                .allowedOrigins(allowedOrigins);
     }
 
 }
