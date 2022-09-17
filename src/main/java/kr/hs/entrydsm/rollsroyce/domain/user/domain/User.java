3155f464dccac1f4232544b850c6e3144b16274a
정대현 <eogus_2513@dsm.hs.kr>
2022-09-17 22:04:43 +0900

    :recycle: :: import 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/User.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/User.java
@@ -16,7 +16,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.DynamicInsert;
 
