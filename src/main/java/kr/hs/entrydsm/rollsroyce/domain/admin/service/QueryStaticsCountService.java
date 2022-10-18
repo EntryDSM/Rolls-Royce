d040ba917fcd55fbaa2556af22791bfbb9b1f347
KimBeomJin <sdpthf@naver.com>
2022-10-18 10:19:51 +0900

    ♻️ :: 정렬

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/QueryStaticsCountService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/QueryStaticsCountService.java
@@ -1,15 +1,12 @@
 package kr.hs.entrydsm.rollsroyce.domain.admin.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.StaticsCountResponse;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserCustomRepository;
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -21,9 +18,7 @@ public class QueryStaticsCountService {
         List<StaticsCountResponse> responseList = new ArrayList<>();
         for (ApplicationType type : ApplicationType.values()) {
             for (int i = 0; i < 2; i++) {
-                int count =
-                        userRepository
-                                .queryStaticsCount(type, i != 0).size();
+                int count = userRepository.queryStaticsCount(type, i != 0).size();
                 responseList.add(
                         new StaticsCountResponse(type, i != 0, count)
                 );
