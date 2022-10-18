4309a9f8bfc1adf738f10ac78b1a2757e425f0cf
정대현 <eogus_2513@dsm.hs.kr>
2022-10-18 11:55:57 +0900

    🔀 :: 최종 제출한 값만 가지고 오도록 설정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/ScoreCustomRepositoryImpl.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/ScoreCustomRepositoryImpl.java
@@ -1,10 +1,12 @@
 package kr.hs.entrydsm.rollsroyce.domain.score.domain.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
 import kr.hs.entrydsm.rollsroyce.domain.score.domain.Score;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.types.ApplicationType;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
 import static kr.hs.entrydsm.rollsroyce.domain.score.domain.QScore.score;
 import static kr.hs.entrydsm.rollsroyce.domain.status.domain.QStatus.status;
 import static kr.hs.entrydsm.rollsroyce.domain.user.domain.QUser.user;
@@ -18,10 +20,12 @@ public class ScoreCustomRepositoryImpl implements ScoreCustomRepository {
     public List<Score> queryScoreByApplicationTypeAndIsDaejeon(ApplicationType applicationType, boolean isDaejeon) {
         return jpaQueryFactory.selectFrom(score)
                 .innerJoin(score.user, user)
-                .innerJoin(status)
-                .on(status.user.eq(user))
-                .where(user.applicationType.eq(applicationType))
-                .where(user.isDaejeon.eq(isDaejeon))
+                .innerJoin(status.user, user)
+                .where(
+                        user.applicationType.eq(applicationType),
+                        user.isDaejeon.eq(isDaejeon),
+                        status.isSubmitted.eq(Boolean.TRUE)
+                )
                 .orderBy(score.totalScore.desc())
                 .fetch();
     }
