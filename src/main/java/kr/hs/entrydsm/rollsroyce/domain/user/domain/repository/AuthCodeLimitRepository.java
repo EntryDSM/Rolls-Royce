931ac45bdb9e2a856de195a8146d8a5648846723
seongwon <seongwon1290@dsm.hs.kr>
2023-01-06 16:01:25 +0900

    ⚰️ :: 인증코드 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/AuthCodeLimitRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.hs.entrydsm.rollsroyce.domain.user.domain.repository;
-
-import kr.hs.entrydsm.rollsroyce.domain.user.domain.AuthCodeLimit;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthCodeLimitRepository extends CrudRepository<AuthCodeLimit, String> {
-}
