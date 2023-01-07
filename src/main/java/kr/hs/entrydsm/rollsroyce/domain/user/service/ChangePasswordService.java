fab0e4bedf3687f0a304e94437730357b2ca2215
seongwon <seongwon1290@dsm.hs.kr>
2023-01-07 15:07:17 +0900

    ♻️ :: 기존 비밀번호 변경 로직 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/ChangePasswordService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/ChangePasswordService.java
@@ -1,8 +1,6 @@
 package kr.hs.entrydsm.rollsroyce.domain.user.service;
 
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.repository.UserRepository;
-import kr.hs.entrydsm.rollsroyce.domain.user.exception.UserNotFoundException;
-import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserAuthCodeFacade;
 import kr.hs.entrydsm.rollsroyce.domain.user.presentation.dto.request.PasswordRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,21 +12,10 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChangePasswordService {
 
     private final PasswordEncoder passwordEncoder;
-
-    private final UserAuthCodeFacade authCodeFacade;
     private final UserRepository userRepository;
 
     @Transactional
     public void execute(PasswordRequest request) {
-        String email = request.getEmail();
 
-        userRepository.findByEmail(email)
-                .filter(user -> authCodeFacade.checkVerified(
-                        authCodeFacade.isVerified(email)
-                ))
-                .map(user -> user.updatePassword(passwordEncoder.encode(request.getNewPassword())))
-                .map(userRepository::save)
-                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
     }
-
 }
