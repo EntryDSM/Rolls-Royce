bada672a0af6e1474c5293accd97ddb753df2e8f
seongwon <seongwon1290@dsm.hs.kr>
2023-01-10 16:49:53 +0900

    ♻️ :: 조회 로직 서비스에서 수행
    원래는 엔티티에서 메소드 만들었음

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/service/QueryInformationService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/service/QueryInformationService.java
@@ -1,6 +1,9 @@
 package kr.hs.entrydsm.rollsroyce.domain.application.service;
 
 import kr.hs.entrydsm.rollsroyce.domain.application.presentation.dto.response.QueryInformationResponse;
+import kr.hs.entrydsm.rollsroyce.domain.entry_info.domain.EntryInfo;
+import kr.hs.entrydsm.rollsroyce.domain.entry_info.facade.EntryInfoFacade;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
 import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
 import kr.hs.entrydsm.rollsroyce.global.utils.s3.S3Util;
 import lombok.RequiredArgsConstructor;
@@ -13,13 +16,31 @@ public class QueryInformationService {
     private final S3Util s3Util;
 
     private final UserFacade userFacade;
+    private final EntryInfoFacade entryInfoFacade;
 
     public QueryInformationResponse execute() {
-        QueryInformationResponse response = userFacade.queryInformation();
+        User user = userFacade.getCurrentUser();
+        EntryInfo entryInfo = entryInfoFacade.getCurrentEntryInfo();
+        QueryInformationResponse response = queryInformation(entryInfo);
 
         response.setPhotoFileName(s3Util.generateObjectUrl(response.getPhotoFileName()));
 
         return response;
+    }
+
+    private QueryInformationResponse queryInformation(EntryInfo entryInfo) {
+        return QueryInformationResponse.builder()
+                .address(entryInfo.getAddress())
+                .parentTel(entryInfo.getParentTel())
+                .sex(String.valueOf(entryInfo.getSex()))
+                .telephoneNumber(entryInfo.getUserTelephoneNumber())
+                .name(entryInfo.getUserName())
+                .detailAddress(entryInfo.getDetailAddress())
+                .parentName(entryInfo.getParentName())
+                .postCode(entryInfo.getPostCode())
+                .photoFileName(entryInfo.getPhotoFileName())
+                .birthday(String.valueOf(entryInfo.getBirthday()))
+                .build();
     }
 
 }
