c82bfa950e2ef7dc8e08bbd30bec0eb1454cc574
seongwon <seongwon1290@dsm.hs.kr>
2023-01-10 16:42:52 +0900

    📑 :: EntryInfoFacade

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/entry_info/facade/EntryInfoFacade.java
@@ -0,0 +1,33 @@
+package kr.hs.entrydsm.rollsroyce.domain.entry_info.facade;
+
+import kr.hs.entrydsm.rollsroyce.domain.entry_info.domain.EntryInfo;
+import kr.hs.entrydsm.rollsroyce.domain.entry_info.domain.repository.EntryInfoRepository;
+import kr.hs.entrydsm.rollsroyce.domain.entry_info.exception.EntryInfoNotFoundException;
+import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
+import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+
+@Component
+@RequiredArgsConstructor
+public class EntryInfoFacade {
+    private final EntryInfoRepository entryInfoRepository;
+    private final UserFacade userFacade;
+
+    public Long getCurrentReceiptCode() {
+        return getCurrentEntryInfo().getReceiptCode();
+    }
+    public EntryInfo getCurrentEntryInfo() {
+        User user = userFacade.getCurrentUser();
+        if(user.getEntryInfo() == null) {
+            throw EntryInfoNotFoundException.EXCEPTION;
+        }
+        return user.getEntryInfo();
+    }
+
+    public EntryInfo getEntryInfoByCode(Long receiptCode) {
+        return entryInfoRepository.findById(receiptCode)
+                .orElseThrow(() -> EntryInfoNotFoundException.EXCEPTION);
+    }
+}
