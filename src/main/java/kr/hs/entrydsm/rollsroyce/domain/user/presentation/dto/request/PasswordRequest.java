3575f7560524bd8f6587967ff071838c8dd909ec
seongwon <seongwon1290@dsm.hs.kr>
2023-01-11 14:11:56 +0900

    ♻️ :: 글자 형식 변경

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/dto/request/PasswordRequest.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/dto/request/PasswordRequest.java
@@ -14,7 +14,7 @@ public class PasswordRequest {
 
     @NotBlank(message = "telephone_number는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
     @Email(message = "이메일 형식이 올바르지 않습니다.")
-    private String telephone_number;
+    private String telephoneNumber;
 
     @NotBlank(message = "new_password는 Null 또는 공백 또는 띄어쓰기를 허용하지 않습니다.")
     @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;<=>?@＼^_`{|}~]{8,32}$",
