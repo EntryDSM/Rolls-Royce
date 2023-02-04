101f9689bc1aa1a41e397a5d0415a1261f6e9d64
정대현 <eogus_2513@dsm.hs.kr>
2023-02-04 14:46:31 +0900

    ♻️ :: error 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/QueryStaticsCountService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/service/QueryStaticsCountService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.StaticsCountResponse;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.repository.EntryInfoCustomRepository;
+import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.repository.EntryInfoRepository;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.types.ApplicationType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,13 +13,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class QueryStaticsCountService {
 
-    private final EntryInfoCustomRepository userRepository;
+    private final EntryInfoRepository entryInfoRepository;
 
     public List<StaticsCountResponse> execute() {
         List<StaticsCountResponse> responseList = new ArrayList<>();
         for (ApplicationType type : ApplicationType.values()) {
             for (int i = 0; i < 2; i++) {
-                int count = userRepository.queryStaticsCount(type, i != 0).size();
+                int count = entryInfoRepository.queryStaticsCount(type, i != 0).size();
                 responseList.add(
                         new StaticsCountResponse(type, i != 0, count)
                 );
--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/entryinfo/domain/repository/EntryInfoRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/entryinfo/domain/repository/EntryInfoRepository.java
@@ -2,6 +2,8 @@ package kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.repository;
 
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.EntryInfo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EntryInfoRepository extends CrudRepository<EntryInfo, Long>, EntryInfoCustomRepository {
 }
