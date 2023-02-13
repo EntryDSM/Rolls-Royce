cb2a0edd5f6326f9d47a93afb72a7f9ca81129f4
주영 <jyk102999@dsm.hs.kr>
2023-02-13 19:51:20 +0900

    ⚡️ :: length 검사 추가

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/presentation/dto/request/CreateQnaRequest.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/presentation/dto/request/CreateQnaRequest.java
@@ -6,14 +6,17 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateQnaRequest {
     @NotBlank(message = "title은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @Size(max = 100, message = "title은 최대 100자까지 가능합니다.")
     private String title;
 
     @NotBlank(message = "content은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @Size(max = 5000, message = "content는 최대 5000자까지 가능합니다.")
     private String content;
 
     @NotNull
