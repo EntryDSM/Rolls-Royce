1ab124e6ec0c187c2ff9ac1215b075713bdf28e1
주영 <jyk102999@dsm.hs.kr>
2023-02-13 15:06:25 +0900

    ⚡️ :: public 추가

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/GraduationCaseCustomRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/score/domain/repository/GraduationCaseCustomRepository.java
@@ -4,7 +4,7 @@ import kr.hs.entrydsm.rollsroyce.domain.score.domain.GraduationCase;
 
 import java.util.List;
 
-interface GraduationCaseCustomRepository {
+public interface GraduationCaseCustomRepository {
     List<GraduationCase> findAllByGraduationCase(String koreanGrade, String socialGrade, String historyGrade, String mathGrade, String scienceGrade,
                                                  String techAndHomeGrade, String englishGrade, Integer volunteerTime, Integer latenessCount,
                                                  Integer dayAbsenceCount, Integer earlyLeaveCount, Integer lectureAbsenceCount);
