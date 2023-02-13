195fbd5cefaaa840b2f155910ef135fa708addf0
주영 <jyk102999@dsm.hs.kr>
2023-02-13 20:11:15 +0900

    ⚡️ :: import * 변경

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/domain/Qna.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/domain/Qna.java
@@ -1,9 +1,19 @@
 package kr.hs.entrydsm.rollsroyce.domain.qna.domain;
 
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
