439de4174c9b0d1b340c9730769f8d9a194c2934
seongwon <seongwon1290@dsm.hs.kr>
2023-03-22 19:22:41 +0900

    ♻️ :: ReserveLink 오류 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/reserve/service/GetReserveLinkService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/reserve/service/GetReserveLinkService.java
@@ -1,14 +1,12 @@
 package kr.hs.entrydsm.rollsroyce.domain.reserve.service;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
 public class GetReserveLinkService {
     @Value("${reserve.link}")
-    private final String reserveLink;
+    private String reserveLink;
 
     public String execute() {
         return reserveLink;
