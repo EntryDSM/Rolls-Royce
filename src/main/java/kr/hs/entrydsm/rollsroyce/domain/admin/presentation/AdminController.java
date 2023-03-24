290eb7a8b39c6c33566c4af2b98edbf0c1c50677
주영 <jyk102999@dsm.hs.kr>
2023-03-24 23:14:53 +0900

    🐛 :: Build Error 해결
    Spotless 해결

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/AdminController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/admin/presentation/AdminController.java
@@ -1,17 +1,7 @@
 package kr.hs.entrydsm.rollsroyce.domain.admin.presentation;
 
-import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.CreateReplyRequest;
-import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.GetApplicantsRequest;
-import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.ApplicantsResponse;
-import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.StaticsCountResponse;
-import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.StaticsScoreResponse;
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.CreateReplyService;
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.DeleteAllTablesService;
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.DeleteQnaService;
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.GetApplicantsService;
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.QueryStaticsCountService;
-import kr.hs.entrydsm.rollsroyce.domain.admin.service.QueryStaticsScore;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,8 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
 import java.util.List;
+
+import javax.validation.Valid;
+
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.CreateReplyRequest;
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.request.GetApplicantsRequest;
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.ApplicantsResponse;
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.StaticsCountResponse;
+import kr.hs.entrydsm.rollsroyce.domain.admin.presentation.dto.response.StaticsScoreResponse;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.CreateReplyService;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.DeleteAllTablesService;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.DeleteQnaService;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.GetApplicantsService;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.QueryStaticsCountService;
+import kr.hs.entrydsm.rollsroyce.domain.admin.service.QueryStaticsScore;
 
 @RequiredArgsConstructor
 @RequestMapping("/admin")
