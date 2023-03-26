dbdc6e20c20b5f5e9e25c8b5aa490d7a78e87d95
주영 <jyk102999@dsm.hs.kr>
2023-03-26 14:29:35 +0900

    ♻️ :: import * 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/presentation/QnaController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/presentation/QnaController.java
@@ -3,7 +3,13 @@ package kr.hs.entrydsm.rollsroyce.domain.qna.presentation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
