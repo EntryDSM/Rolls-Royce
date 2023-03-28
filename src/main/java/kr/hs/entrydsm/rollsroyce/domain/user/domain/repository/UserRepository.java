0ebd37e64083ffa0f3a9e0793c2f61e50d7b8e78
seongwon <seongwon1290@dsm.hs.kr>
2023-03-28 09:32:04 +0900

    ⚡️ :: findByTelephoneNumberAndName() 추가

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/domain/repository/UserRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 public interface UserRepository extends CrudRepository<User, Long> {
     Optional<User> findByTelephoneNumber(String telephoneNumber);
     boolean existsByTelephoneNumber(String telephoneNumber);
+
+    User findByTelephoneNumberAndName(String telephoneNumber, String name);
 }
