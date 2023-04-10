233ceefb9d68d0c3209cf770aaf3de185f72d27f
주영 <jyk102999@dsm.hs.kr>
2023-04-10 20:10:33 +0900

    ⚡️ :: message 내용 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/notice/presentation/dto/request/CreateNoticeRequest.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/notice/presentation/dto/request/CreateNoticeRequest.java
@@ -15,7 +15,7 @@ public class CreateNoticeRequest {
     private String title;
 
     @NotBlank(message = "content은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Size(max = 5000, message = "content은 최대 100자까지 가능합니다.")
+    @Size(max = 5000, message = "content은 최대 5000자까지 가능합니다.")
     private String content;
 
     @NotBlank(message = "type은 null, 공백, 띄어쓰기를 허용하지 않습니다.")
