db45b0da070a6d255ed7e43f65bc731b4ea220cf
seongwon <seongwon1290@dsm.hs.kr>
2023-04-16 01:50:25 +0900

    ♻️ :: PASS 관련 예외 추가

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/auth/exception/InvalidPassException.java
@@ -0,0 +1,14 @@
+package kr.hs.entrydsm.rollsroyce.domain.auth.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class InvalidPassException extends RollsException {
+    public static final RollsException EXCEPTION =
+            new InvalidPassException();
+
+    private InvalidPassException() {
+        super(ErrorCode.INVALID_PASS);
+    }
+}
+
---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/auth/exception/PassInfoNotFoundException.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.rollsroyce.domain.auth.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class PassInfoNotFoundException extends RollsException {
+    public static final RollsException EXCEPTION =
+            new PassInfoNotFoundException();
+
+    private PassInfoNotFoundException() {
+        super(ErrorCode.PASS_INFO_NOT_FOUND);
+    }
+}
