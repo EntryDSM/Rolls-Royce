418731d392f9063b8654f70728d0f489096e5fe5
주영 <jyk102999@dsm.hs.kr>
2023-05-10 01:24:28 +0900

    🔀 :: (#521) 답변 조회 API 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/service/QueryDetailsQnaService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/qna/service/QueryDetailsQnaService.java
@@ -5,8 +5,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import kr.hs.entrydsm.rollsroyce.domain.admin.domain.Reply;
+import kr.hs.entrydsm.rollsroyce.domain.admin.domain.repository.ReplyRepository;
 import kr.hs.entrydsm.rollsroyce.domain.qna.domain.Qna;
 import kr.hs.entrydsm.rollsroyce.domain.qna.exception.AccessDeniedQnaException;
+import kr.hs.entrydsm.rollsroyce.domain.qna.exception.ReplyNotFoundException;
 import kr.hs.entrydsm.rollsroyce.domain.qna.facade.QnaFacade;
 import kr.hs.entrydsm.rollsroyce.domain.qna.presentation.dto.response.QueryDetailsQnaResponse;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
@@ -17,6 +20,7 @@ import kr.hs.entrydsm.rollsroyce.domain.user.facade.UserFacade;
 public class QueryDetailsQnaService {
     private final QnaFacade qnaFacade;
     private final UserFacade userFacade;
+    private final ReplyRepository replyRepository;
 
     @Transactional(readOnly = true)
     public QueryDetailsQnaResponse execute(Long questionId) {
@@ -27,19 +31,33 @@ public class QueryDetailsQnaService {
             throw AccessDeniedQnaException.EXCEPTION;
         }
 
-        return getQna(questionId);
+        return QueryDetailsQnaResponse.builder()
+                .qna(getQna(qna.getId()))
+                .reply(getReply(qna.getId()))
+                .build();
     }
 
-    private QueryDetailsQnaResponse getQna(Long qnaId) {
+    private QueryDetailsQnaResponse.QnaDto getQna(Long qnaId) {
         Qna qna = qnaFacade.getQnaById(qnaId);
 
-        return QueryDetailsQnaResponse.builder()
+        return QueryDetailsQnaResponse.QnaDto.builder()
                 .id(qna.getId())
                 .title(qna.getTitle())
                 .content(qna.getContent())
                 .username(qna.getUser().getName())
                 .isReplied(qna.getIsReplied())
                 .createdAt(qna.getCreatedAt())
+                .build();
+    }
+
+    private QueryDetailsQnaResponse.ReplyDto getReply(Long qnaId) {
+        Reply reply = replyRepository.findByQnaId(qnaId).orElseThrow(() -> ReplyNotFoundException.EXCEPTION);
+
+        return QueryDetailsQnaResponse.ReplyDto.builder()
+                .id(reply.getId())
+                .title(reply.getTitle())
+                .content(reply.getContent())
+                .createdAt(reply.getCreatedAt())
                 .build();
     }
 }
