70f6e84bd2d88215cab96677a3571fed40bbb149
seongwon <seongwon1290@dsm.hs.kr>
2023-05-11 17:26:58 +0900

    📑 :: 합격 미발표 예외 추가

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/pass/exception/AdmissionUnavailableException.java
@@ -0,0 +1,13 @@
+package kr.hs.entrydsm.rollsroyce.domain.pass.exception;
+
+import kr.hs.entrydsm.rollsroyce.global.error.exception.ErrorCode;
+import kr.hs.entrydsm.rollsroyce.global.error.exception.RollsException;
+
+public class AdmissionUnavailableException extends RollsException {
+    public static final RollsException EXCEPTION =
+            new AdmissionUnavailableException();
+
+    private AdmissionUnavailableException() {
+        super(ErrorCode.ADMISSION_UNAVAILABLE);
+    }
+}
