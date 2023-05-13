3afc457625b0197e6fcc9778fce240465f583485
seongwon <seongwon1290@dsm.hs.kr>
2023-05-13 14:10:32 +0900

    ⚡️ :: Faq 전체조회 API

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/faq/presentation/dto/response/QueryFaqResponse.java
@@ -0,0 +1,16 @@
+package kr.hs.entrydsm.rollsroyce.domain.faq.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+import kr.hs.entrydsm.rollsroyce.domain.faq.domain.type.FaqType;
+
+@Getter
+@Builder
+public class QueryFaqResponse {
+    private final String title;
+    private final LocalDateTime createdAt;
+    private final FaqType faqType;
+}
