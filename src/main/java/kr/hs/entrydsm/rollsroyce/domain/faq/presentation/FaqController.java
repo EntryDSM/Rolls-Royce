7ce2cd40a534471339f40e40f30548464f273b66
seongwon <seongwon1290@dsm.hs.kr>
2023-05-13 14:11:08 +0900

    📑 :: FaqController

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/faq/presentation/FaqController.java
@@ -0,0 +1,57 @@
+package kr.hs.entrydsm.rollsroyce.domain.faq.presentation;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import kr.hs.entrydsm.rollsroyce.domain.faq.presentation.dto.request.CreateFaqRequest;
+import kr.hs.entrydsm.rollsroyce.domain.faq.presentation.dto.request.UpdateFaqRequest;
+import kr.hs.entrydsm.rollsroyce.domain.faq.presentation.dto.response.QueryFaqInfoResponse;
+import kr.hs.entrydsm.rollsroyce.domain.faq.presentation.dto.response.QueryFaqResponse;
+import kr.hs.entrydsm.rollsroyce.domain.faq.service.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/faq")
+public class FaqController {
+    private final CreateFaqService createFaqService;
+    private final DeleteFaqService deleteFaqService;
+    private final QueryFaqInfoService queryFaqInfoService;
+    private final QueryFaqListService queryFaqListService;
+    private final QueryFaqListByTypeService queryFaqListByTypeService;
+    private final UpdateFaqService updateFaqService;
+
+    @PostMapping
+    public void createFaq(@RequestBody @Valid CreateFaqRequest request) {
+        createFaqService.execute(request);
+    }
+
+    @PatchMapping("/{faq-id}")
+    public void updateFaq(@PathVariable("faq-id") Long id, @RequestBody @Valid UpdateFaqRequest request) {
+        updateFaqService.execute(id, request);
+    }
+
+    @DeleteMapping("/{faq-id}")
+    public void deleteFaq(@PathVariable("faq-id") Long id) {
+        deleteFaqService.execute(id);
+    }
+
+    @GetMapping("/{faq-id}")
+    public QueryFaqInfoResponse execute(@PathVariable("faq-id") Long id) {
+        return queryFaqInfoService.execute(id);
+    }
+
+    @GetMapping
+    public List<QueryFaqResponse> execute(@RequestParam("type") String type) {
+        return queryFaqListByTypeService.execute(type);
+    }
+
+    @GetMapping("/all")
+    public List<QueryFaqResponse> execute() {
+        return queryFaqListService.execute();
+    }
+}
