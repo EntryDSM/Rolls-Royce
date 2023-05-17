77290e0427730971d048ef67c3b96980f52d8756
주영 <jyk102999@dsm.hs.kr>
2023-05-18 01:18:48 +0900

    ♻️ :: 안쓰는 필드 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/screen/service/UpdateScreenService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/screen/service/UpdateScreenService.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import kr.hs.entrydsm.rollsroyce.domain.admin.facade.AdminFacade;
 import kr.hs.entrydsm.rollsroyce.domain.screen.domain.Screen;
 import kr.hs.entrydsm.rollsroyce.domain.screen.domain.repository.ScreenRepository;
 import kr.hs.entrydsm.rollsroyce.domain.screen.exception.ScreenNotFoundException;
@@ -16,7 +15,6 @@ import kr.hs.entrydsm.rollsroyce.global.utils.s3.S3Util;
 @Service
 public class UpdateScreenService {
     private final ScreenRepository screenRepository;
-    private final AdminFacade adminFacade;
     private final S3Util s3Util;
 
     @Transactional
