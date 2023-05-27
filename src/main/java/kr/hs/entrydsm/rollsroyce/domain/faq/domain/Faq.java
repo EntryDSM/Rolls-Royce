fbbb97e12d48ed67c0dbd819da71ddb9f38773a8
seongwon <seongwon1290@dsm.hs.kr>
2023-05-27 14:08:56 +0900

    ♻️ :: import 와일드카드 삭제

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/faq/domain/Faq.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/faq/domain/Faq.java
@@ -5,7 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.OneToOne;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.JoinColumn;
 
 import kr.hs.entrydsm.rollsroyce.domain.admin.domain.Admin;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.BaseTimeEntity;
