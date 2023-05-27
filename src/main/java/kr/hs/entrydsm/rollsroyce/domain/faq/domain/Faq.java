ffd2bdff162d2a5f04fae5b2644aff40dd78d198
seongwon <seongwon1290@dsm.hs.kr>
2023-05-27 21:40:26 +0900

    ♻️ :: lint

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/faq/domain/Faq.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/faq/domain/Faq.java
@@ -5,15 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Column;
-import javax.persistence.OneToOne;
-import javax.persistence.Enumerated;
-import javax.persistence.EnumType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 import kr.hs.entrydsm.rollsroyce.domain.admin.domain.Admin;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.BaseTimeEntity;
