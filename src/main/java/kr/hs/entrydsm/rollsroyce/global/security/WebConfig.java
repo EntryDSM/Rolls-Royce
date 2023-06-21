27579e9836198317d3eff6be027ad0245062d4d2
주영 <jyk102999@dsm.hs.kr>
2023-06-21 11:45:01 +0900

    ♻️ :: 도메인 추가

--- src/main/java/kr/hs/entrydsm/rollsroyce/global/security/WebConfig.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/WebConfig.java
@@ -21,6 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
                         adminOrigin,
                         "http://localhost:3000",
                         "http://localhost:3001",
-                        "http://localhost:3002");
+                        "http://localhost:3002",
+                        "https://auth.entrydsm.hs.kr/login");
     }
 }
