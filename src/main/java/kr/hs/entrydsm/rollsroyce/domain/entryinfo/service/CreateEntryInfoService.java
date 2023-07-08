21ad01a587ebe3321d51ee007ea52fa11ee7a350
주영 <jyk102999@dsm.hs.kr>
2023-07-08 23:26:36 +0900

    🐛 :: 500 버그 해결

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/entryinfo/service/CreateEntryInfoService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/entryinfo/service/CreateEntryInfoService.java
@@ -35,6 +35,7 @@ public class CreateEntryInfoService {
                 .isPrintsArrived(false)
                 .isSubmitted(false)
                 .isFirstRoundPass(false)
+                .isSecondRoundPass(false)
                 .build());
     }
 }
