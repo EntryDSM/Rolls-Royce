2768d9bbf1f864b9705bc603bddf5e17106de1ed
주영 <jyk102999@dsm.hs.kr>
2023-07-13 01:01:17 +0900

    🐛 :: 값 안 나오는 버그 수정

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/UserController.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/presentation/UserController.java
@@ -3,10 +3,21 @@ package kr.hs.entrydsm.rollsroyce.domain.user.presentation;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import kr.hs.entrydsm.rollsroyce.domain.entryinfo.presentation.dto.response.StatusResponse;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.service.CancelEntryService;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.service.CreateEntryInfoService;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.service.EntryStatusService;
@@ -73,7 +84,7 @@ public class UserController {
     }
 
     @GetMapping("/status")
-    public void queryEntryStatus() {
-        entryStatusService.execute();
+    public StatusResponse queryEntryStatus() {
+        return entryStatusService.execute();
     }
 }
