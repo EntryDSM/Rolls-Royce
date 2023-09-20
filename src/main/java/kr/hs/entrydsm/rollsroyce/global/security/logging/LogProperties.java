4565fe732c973cf2720dae64ad0d4f4408f17768
주영 <jyk102999@dsm.hs.kr>
2023-09-21 08:37:06 +0900

    ♻️ :: Log

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/security/logging/LogProperties.java
@@ -0,0 +1,20 @@
+package kr.hs.entrydsm.rollsroyce.global.security.logging;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "rolls-royce.logging")
+public class LogProperties {
+
+    private String name;
+
+    private String path;
+
+    private int size;
+}
