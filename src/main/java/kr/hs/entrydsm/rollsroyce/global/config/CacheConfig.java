0b083001a5e939ef7d8a7a44894542e5a480f573
seongwon <seongwon1290@dsm.hs.kr>
2023-09-23 11:53:23 +0900

    📑 :: CacheConfig

---
+++ src/main/java/kr/hs/entrydsm/rollsroyce/global/config/CacheConfig.java
@@ -0,0 +1,8 @@
+package kr.hs.entrydsm.rollsroyce.global.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {}
