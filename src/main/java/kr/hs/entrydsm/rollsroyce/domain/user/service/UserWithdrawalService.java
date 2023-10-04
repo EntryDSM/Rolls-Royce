228c98edebdfeeb871d43dfe9df8172da8deea93
seongwon <seongwon1290@dsm.hs.kr>
2023-10-05 00:27:52 +0900

    ♻️ :: 회원탈퇴 오류 해결

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/UserWithdrawalService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/UserWithdrawalService.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
-import kr.hs.entrydsm.rollsroyce.domain.application.domain.Graduation;
-import kr.hs.entrydsm.rollsroyce.domain.application.domain.Qualification;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.repository.GraduationRepository;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.repository.QualificationRepository;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.repository.EntryInfoRepository;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.types.EducationalStatus;
-import kr.hs.entrydsm.rollsroyce.domain.score.domain.Score;
 import kr.hs.entrydsm.rollsroyce.domain.score.domain.repository.ScoreRepository;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.repository.StatusRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
@@ -33,25 +30,22 @@ public class UserWithdrawalService {
     @Transactional
     public void execute() {
         User user = userFacade.getCurrentUser();
-        Score score = scoreRepository.findByReceiptCode(user.getEntryInfoReceiptCode());
-
+        Long receiptCode = user.getEntryInfoReceiptCode();
         if (user.getEntryInfo() != null && EducationalStatus.GRADUATE.equals(user.getEntryInfoEducationStatus())
                 || EducationalStatus.PROSPECTIVE_GRADUATE.equals(user.getEntryInfoEducationStatus())) {
-            Graduation graduation = graduationRepository.findByReceiptCode(user.getEntryInfoReceiptCode());
-            scoreRepository.delete(score);
-            graduationRepository.delete(graduation);
-            statusRepository.delete(user.getStatus());
-            entryInfoRepository.delete(user.getEntryInfo());
+            scoreRepository.findById(receiptCode).ifPresent(scoreRepository::delete);
+            graduationRepository.findById(receiptCode).ifPresent(graduationRepository::delete);
+            statusRepository.findById(receiptCode).ifPresent(statusRepository::delete);
+            entryInfoRepository.deleteById(receiptCode);
         } else if (user.getEntryInfo() != null
                 && EducationalStatus.QUALIFICATION_EXAM.equals(user.getEntryInfoEducationStatus())) {
-            Qualification qualification = qualificationRepository.findByReceiptCode(user.getEntryInfoReceiptCode());
-            qualificationRepository.delete(qualification);
-            scoreRepository.delete(score);
-            statusRepository.delete(user.getStatus());
-            entryInfoRepository.delete(user.getEntryInfo());
+            scoreRepository.findById(receiptCode).ifPresent(scoreRepository::delete);
+            qualificationRepository.findById(receiptCode).ifPresent(qualificationRepository::delete);
+            statusRepository.findById(receiptCode).ifPresent(statusRepository::delete);
+            entryInfoRepository.deleteById(receiptCode);
         } else if (user.getEntryInfoReceiptCode() != null) {
-            statusRepository.delete(user.getStatus());
-            entryInfoRepository.delete(user.getEntryInfo());
+            statusRepository.findById(receiptCode).ifPresent(statusRepository::delete);
+            entryInfoRepository.deleteById(receiptCode);
         }
 
         userRepository.delete(user);
