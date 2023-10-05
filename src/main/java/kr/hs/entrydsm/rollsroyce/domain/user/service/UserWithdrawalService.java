3de6a3a2b1359adb2e804fb92eadd69df7323c20
주영 <jyk102999@dsm.hs.kr>
2023-10-05 09:22:18 +0900

    🐛 :: 질문이 있을 때 회원탈퇴 안 되는 버그 해결

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/UserWithdrawalService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/user/service/UserWithdrawalService.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.repository.GraduationRepository;
 import kr.hs.entrydsm.rollsroyce.domain.application.domain.repository.QualificationRepository;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.repository.EntryInfoRepository;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.types.EducationalStatus;
+import kr.hs.entrydsm.rollsroyce.domain.question.domain.Question;
+import kr.hs.entrydsm.rollsroyce.domain.question.domain.repository.QuestionRepository;
 import kr.hs.entrydsm.rollsroyce.domain.score.domain.repository.ScoreRepository;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.repository.StatusRepository;
 import kr.hs.entrydsm.rollsroyce.domain.user.domain.User;
@@ -26,11 +30,13 @@ public class UserWithdrawalService {
     private final ScoreRepository scoreRepository;
     private final GraduationRepository graduationRepository;
     private final QualificationRepository qualificationRepository;
+    private final QuestionRepository questionRepository;
 
     @Transactional
     public void execute() {
         User user = userFacade.getCurrentUser();
         Long receiptCode = user.getEntryInfoReceiptCode();
+        List<Question> questions = questionRepository.findAllByUserOrderByCreatedAtDesc(user);
         if (user.getEntryInfo() != null && EducationalStatus.GRADUATE.equals(user.getEntryInfoEducationStatus())
                 || EducationalStatus.PROSPECTIVE_GRADUATE.equals(user.getEntryInfoEducationStatus())) {
             scoreRepository.findById(receiptCode).ifPresent(scoreRepository::delete);
@@ -46,6 +52,10 @@ public class UserWithdrawalService {
         } else if (user.getEntryInfoReceiptCode() != null) {
             statusRepository.findById(receiptCode).ifPresent(statusRepository::delete);
             entryInfoRepository.deleteById(receiptCode);
+        }
+
+        if (questions != null) {
+            questionRepository.deleteAll(questions);
         }
 
         userRepository.delete(user);
