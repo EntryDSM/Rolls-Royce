ccadbbbd1c784dc4a63676331158917eb8b5ef20
seongwon <seongwon1290@dsm.hs.kr>
2023-10-10 08:22:51 +0900

    ♻️ :: 점수 검증

--- src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/service/FinalSubmitService.java
+++ src/main/java/kr/hs/entrydsm/rollsroyce/domain/application/service/FinalSubmitService.java
@@ -10,6 +10,7 @@ import kr.hs.entrydsm.rollsroyce.domain.application.exception.ProcessNotComplete
 import kr.hs.entrydsm.rollsroyce.domain.application.facade.ApplicationFacade;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.domain.EntryInfo;
 import kr.hs.entrydsm.rollsroyce.domain.entryinfo.facade.EntryInfoFacade;
+import kr.hs.entrydsm.rollsroyce.domain.score.domain.repository.ScoreRepository;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.Status;
 import kr.hs.entrydsm.rollsroyce.domain.status.domain.facade.StatusFacade;
 import kr.hs.entrydsm.rollsroyce.domain.status.exception.AlreadySubmitException;
@@ -17,18 +18,17 @@ import kr.hs.entrydsm.rollsroyce.domain.status.exception.AlreadySubmitException;
 @RequiredArgsConstructor
 @Service
 public class FinalSubmitService {
-
     private final ApplicationFacade applicationFacade;
-
     private final StatusFacade statusFacade;
-
     private final EntryInfoFacade entryInfoFacade;
+    private final ScoreRepository scoreRepository;
 
     @Transactional
     public void execute() {
         EntryInfo entryInfo = entryInfoFacade.getCurrentEntryInfo();
 
-        if (entryInfo.hasEmptyInfo() || checkApplication(entryInfo)) throw ProcessNotCompletedException.EXCEPTION;
+        if (entryInfo.hasEmptyInfo() || checkApplication(entryInfo) || checkScore(entryInfo))
+            throw ProcessNotCompletedException.EXCEPTION;
 
         Status status = statusFacade.getStatusByReceiptCode(entryInfo.getReceiptCode());
 
@@ -42,5 +42,9 @@ public class FinalSubmitService {
                 applicationFacade.getApplication(entryInfo.getReceiptCode(), entryInfo.getEducationalStatus());
 
         return application.hasEmptyInfo();
+    }
+
+    private boolean checkScore(EntryInfo entryInfo) {
+        return scoreRepository.findById(entryInfo.getReceiptCode()).isEmpty(); // todo 나중에 hasEmptyInfo 추가하는게 좋을듯
     }
 }
